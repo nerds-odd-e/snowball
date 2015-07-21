574a5a644d2aad3caa86581886b5237fb76fc0de
uhura <uhura@local.com>
2015-07-21 18:58:09 +0800

    add condition connection in db

---
+++ src/main/java/com/odde/massivemailer/service/ContactService.java
@@ -0,0 +1,11 @@
+package com.odde.massivemailer.service;
+
+public class ContactService {
+
+	public String localhost = "localhost";
+	public String username = "root";
+	public String password = "admin";
+	public String URL = "jdbc:mysql://localhost/oddemail";
+	public String driverName = "com.mysql.jdbc.Driver";
+	public String selectMail = "SELECT id, name FROM mail";
+}
