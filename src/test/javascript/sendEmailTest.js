f9bd278f4f9d23c2fed4fbb537cb58e9612f5d3d
saavik <saavik@gmail.com>
2015-07-22 19:25:58 +0800

    add test case

--- src/test/javascript/sendEmailTest.js
+++ src/test/javascript/sendEmailTest.js
@@ -1,5 +1,8 @@
 describe('check blank element',function(){
-  it('should be true if blank',function(){
+  it('should be true if it is blank',function(){
     expect(isBlank(" ")).toBe(true);
   });
+  it('should be false if it is not blank',function(){
+	    expect(isBlank("not blank")).toBe(false);
+	  });
 });
