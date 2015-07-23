ebab2b9a615ccafbaa6feedb181fc7405509e4ab
saavik <saavik@gmail.com>
2015-07-23 15:30:37 +0800

    add test case and refractor send email module

--- src/test/javascript/sendEmailTest.js
+++ src/test/javascript/sendEmailTest.js
@@ -1,8 +1,46 @@
-describe('check blank element',function(){
-  it('should be true if it is blank',function(){
-    expect(isBlank(" ")).toBe(true);
+describe('test isBlank function',function(){
+	
+  it('should return true when pass blank value',function(){
+	  expect(isBlank(" ")).toBe(true);
   });
-  it('should be false if it is not blank',function(){
-	    expect(isBlank("not blank")).toBe(false);
-	  });
+  
+  it('should return false when pass not blank value',function(){
+	   expect(isBlank("mail@mail.com")).toBe(false);
+ });
 });
+
+describe('test checkInputElement function', function(){
+	var rootId = "testContainer";
+	var markup = "<input type='text' id='recipient'> " +
+			"<input type='text' id='content'> " +
+			"<input type='text' id='subject'> " +
+			"<button type='button' id='send_button' value='send'>Send</button>";
+	beforeEach(function(){
+		var container = document.createElement('div');
+		container.setAttribute('id', rootId);
+		document.body.appendChild(container);
+		container.innerHTML = markup;
+	});
+	
+	afterEach(function() {
+		var container = document.getElementById(rootId);
+		container.parentNode.removeChild(container);
+	});
+
+	it('should disable send button when all fields are blank',function(){
+        checkInputElement();
+        expect(document.getElementById("send_button").disabled).toBe(true);
+	});
+	
+	it('should enable send button when all fields are not blank',function(){
+        document.getElementById("recipient").value = "mail@mail.com";
+        document.getElementById("subject").value = "Greeting!!!";
+        document.getElementById("content").value = "Hello World.";
+        
+        checkInputElement();
+        
+        expect(document.getElementById("send_button").disabled).toBe(false);
+	});
+	
+});
+
