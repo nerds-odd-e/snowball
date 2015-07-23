ebf1c77b039b385c6b31ca257cd491b767e43119
uhura <uhura@local.com>
2015-07-23 18:53:12 +0700

    Commit ContactService

--- src/main/java/com/odde/massivemailer/service/ContactService.java
+++ src/main/java/com/odde/massivemailer/service/ContactService.java
@@ -1,11 +1,68 @@
 package com.odde.massivemailer.service;
 
-public class ContactService {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-	public String localhost = "localhost";
-	public String username = "root";
-	public String password = "admin";
-	public String URL = "jdbc:mysql://localhost/oddemail";
-	public String driverName = "com.mysql.jdbc.Driver";
-	public String selectMail = "SELECT id, name FROM mail";
+import javax.naming.spi.DirStateFactory.Result;
+
+import com.odde.massivemailer.model.ContactPerson;
+
+public class ContactService {
+	private String selectMailSql = "SELECT id, name FROM mail";
+	private String dbName = "jdbc:sqlite:oddemail.db";
+	private List<ContactPerson> contactList;
+
+	public Connection connectDB(String url) throws ClassNotFoundException, SQLException {
+		Connection connection = DriverManager.getConnection(url);
+		Statement statement = connection.createStatement();
+		statement.executeUpdate(
+				"CREATE TABLE IF NOT EXISTS mail (id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL, name VARCHAR(50) NOT NULL)");
+		return connection;
+	}
+
+	public List<ContactPerson> getContactList() {
+		Connection connection;
+		ResultSet resultSet = null;
+		try {
+			connection = this.connectDB(dbName);
+			Statement statement = connection.createStatement();
+			resultSet = statement.executeQuery(this.selectMailSql);
+			populateContactList(resultSet);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+
+	public int addNewContact(String name) {
+		Connection connection;
+		Statement statement;
+		int rowAffected = 0;
+		try {
+			connection = this.connectDB(dbName);
+			statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT name FROM mail WHERE name='" + name + "'");
+			if(!resultSet.next())
+				rowAffected = statement.executeUpdate("INSERT INTO mail(name) VALUES ('"+ name +"')");				
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return rowAffected;
+	}
+	
+	
+	private void populateContactList(ResultSet resultSet) throws SQLException{
+		contactList = new ArrayList<ContactPerson>();
+		while (resultSet.next()) {
+			ContactPerson contact = new ContactPerson();
+			contact.setId(resultSet.getInt("id"));
+			contact.setName(resultSet.getString("name"));
+			contactList.add(contact);
+		}
+	}
 }
--- src/test/java/com/odde/massivemailer/service/spec/ContactServiceTest.java
+++ src/test/java/com/odde/massivemailer/service/spec/ContactServiceTest.java
@@ -1,32 +1,59 @@
 package com.odde.massivemailer.service.spec;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.odde.massivemailer.model.ContactPerson;
 import com.odde.massivemailer.service.ContactService;
 
 public class ContactServiceTest {
 
-	ContactService service = new ContactService();
-	
-	@Test
-	public void CheckPreCondition() {
-		assertEquals("localhost", service.localhost);
-		assertEquals("root", service.username);
-		assertEquals("admin", service.password);
-		assertEquals("jdbc:mysql://localhost/oddemail", service.URL);
-	}
-	
-	@Test
-	public void CheckRegisterStep() {
-		assertEquals("com.mysql.jdbc.Driver", service.driverName);
-		
+	private ContactService service = new ContactService();
+	private Connection connection;
+	private Statement stmt;
+	private ResultSet resultSet;
+	private List<ContactPerson> contactList;
+
+	@Before
+	public void setupConnection() throws ClassNotFoundException, SQLException {
+		connection = service.connectDB("jdbc:sqlite:oddemail.db");
+		stmt = connection.createStatement();
+		stmt.executeUpdate("DELETE FROM mail");
+		stmt.executeUpdate("INSERT INTO mail(name) VALUES ('name 1')");
 	}
 
 	@Test
-	public void CheckSqlSelect() {
-		assertEquals("SELECT id, name FROM mail", service.selectMail);
-		
+	public void checkContactReturned() throws SQLException, ClassNotFoundException {
+		contactList = service.getContactList();
+		for(ContactPerson contact : contactList) {
+			assertEquals("name 1", contact.getName());
+		}
+	}
+	
+	@Test
+	public void addNewContactTest() throws SQLException {
+		int rowAffected = service.addNewContact("AddNewContact");
+		assertEquals(1, rowAffected);
+	}
+	
+	@Test
+	public void addNewContactFailedTest() throws SQLException {
+		int rowAffected = service.addNewContact("name 1");
+		assertEquals(0, rowAffected);
+	}
+	
+	@After
+	public void resetConnection() throws SQLException {
+		stmt.close();
+		connection.close();
 	}
 }
