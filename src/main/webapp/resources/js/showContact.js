889908bce6531ffe6e8484a9ee35d445ac22af2f
bhanumat.w@gmail.com <bhanumat.w@gmail.com>
2015-08-05 14:23:17 +0700

    extract method to render contact and add unit test

--- src/main/webapp/resources/js/showContact.js
+++ src/main/webapp/resources/js/showContact.js
@@ -2,10 +2,20 @@
  * 
  */
 $(document).ready(function() {
-	$.getJSON("showContact", function(json) {
-		var $contactTable = $('#contactTable');
-		$.each(json, function(idx, item) {
-			$contactTable.append('<li class="col-md-1" style="text-align: center">'+item.id+'</li> <li class="col-md-11" style="text-align: center">'+item.email+'</li>');
-		})
-	});
+
+	retrieveContactListFromServer();
+
 });
+
+function retrieveContactListFromServer()
+{
+	$.getJSON("showContact", renderContactList);
+}
+
+function renderContactList(json)
+{
+	var $contactTable = $('#contactTable');
+	$.each(json, function(idx, item) {
+		$contactTable.append('<li class="col-md-1" style="text-align: center">'+item.id+'</li> <li class="col-md-11" style="text-align: center">'+item.email+'</li>');
+	})
+}
