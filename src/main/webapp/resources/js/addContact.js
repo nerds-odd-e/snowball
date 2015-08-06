13371ca0d3e404393626d383ff9d5697c492c50b
Cadet <cadet@ARCHER.local>
2015-08-06 11:39:02 +0700

    show alert message after add contact

--- src/main/webapp/resources/js/addContact.js
+++ src/main/webapp/resources/js/addContact.js
@@ -18,6 +18,7 @@ function submitForm() {
 	$("#email").val("");
 }
 
+
 function checkAddInputElement() {
 	
 	if (isChange($("#email").val())) {
--- src/main/webapp/resources/js/showContact.js
+++ src/main/webapp/resources/js/showContact.js
@@ -29,3 +29,10 @@ function renderContactSelectionList(json, selector)
 		selector.append(contactEmail + item.email+ '</li>');
 	})
 }
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
