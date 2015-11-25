ed925237133948f5e1413112448bcdd593ca3a9b
uhura <uhura@local.com>
2015-11-25 17:46:09 +0700

    use relative path to CustomKeyword script

--- src/test/robotframework/acceptance/CustomKeyword.py
+++ src/test/robotframework/acceptance/CustomKeyword.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def clear_all_contacts():
-	conn = sqlite3.connect('C:\massive_mailer\oddemail.db')
+	conn = sqlite3.connect('..\..\..\..\oddemail.db')
 	c = conn.cursor()
 	c.execute('''delete from mail''')
 	conn.commit()
