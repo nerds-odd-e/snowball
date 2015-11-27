ad4d0589fd03163b1a3634db8d1c4e72856dff47
jannar <jannarodde@gmail.com>
2015-11-27 11:21:38 +0700

    Send Mail by company

--- src/main/java/com/odde/massivemailer/controller/SendMailController.java
+++ src/main/java/com/odde/massivemailer/controller/SendMailController.java
@@ -3,6 +3,7 @@ package com.odde.massivemailer.controller;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -22,15 +23,11 @@ import com.odde.massivemailer.service.impl.SqliteContact;
 
 public class SendMailController extends HttpServlet {
 
+	private SqliteContact sqliteContact;
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-	
-
-		//Mail email = processRequest(req);
-		//MailService mailService = new GMailService();
-		//mailService.setConfiguration(new SMTPConfiguration("myodde@gmail.com", "1234qwer@", "smtp.gmail.com", 587));
-
 		try {
 			Mail email = processRequest(req);
 			MailService mailService = new GMailService();
@@ -42,7 +39,7 @@ public class SendMailController extends HttpServlet {
 			e.printStackTrace();
 			//
 		} catch (SQLException e) {
-			resp.sendRedirect("sendemail.jsp?status=failed&msg=something");
+			resp.sendRedirect("sendemail.jsp?status=failed&msg=Fail");
 			e.printStackTrace();
 		}
 	}
@@ -60,39 +57,52 @@ public class SendMailController extends HttpServlet {
 				String[] aaa = recipient.split(":");
 				List<ContactPerson> contactList;				
 				String company = aaa[1].toString();
-				
-				 
-//				"abc" -> abc
-//				"abc  -> abc
-//				a"bc  -> a"bc
-				
-// error cases				
-//              "a"bc" --> a"bc
-//				"a"bc -> "a"bc
 
-				if(company.startsWith("\"") && company.endsWith("\"")){
-					company = company.substring(1, company.length()-2);
+				if(company.startsWith("\"") && company.endsWith("\"") && countCharOccurrence(company, '"') == 2){ 
+					company = company.substring(1, company.length()-1); 				
+				} else if (company.startsWith("\"") && countCharOccurrence(company, '"') == 1) {
+					company = company.substring(1, company.length());
 				}
-				
-				SqliteContact contactService = new SqliteContact();
+				SqliteContact contactService = getSqliteContact();
 				contactList = contactService.getContactListFromCompany(company);
-
 				if (contactList.isEmpty()) {
 					throw new SQLException() ;
+				}
+				for (ContactPerson contactPerson : contactList) {
+					recipientList.add(contactPerson.getEmail());
 				}
 			}
 			else{
 				recipientList.add(recipient);
 			}
-			
-			
 		}
-
 		email.setContent(req.getParameter("content"));
 		email.setSubject(req.getParameter("subject"));
 		email.setReceipts(recipientList);
 
 		return email;
+	}
+
+	public SqliteContact getSqliteContact() {
+		
+		return (sqliteContact == null) ? sqliteContact = new SqliteContact() : sqliteContact;
+	}
+	
+	public void setSqliteContact(SqliteContact contactService)
+	{
+		sqliteContact = contactService;
+	}
+	
+	
+	private int countCharOccurrence(String input, char countedChar)
+	{
+		int counter = 0;
+		for( int i=0; i < input.length(); i++ ) {
+		    if( input.charAt(i) == countedChar ) {
+		        counter++;
+		    } 
+		}
+		return counter;
 	}
 
 }
--- src/test/java/com/odde/massivemailer/SendMailControllerTest.java
+++ src/test/java/com/odde/massivemailer/SendMailControllerTest.java
@@ -3,6 +3,8 @@ package com.odde.massivemailer;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,33 +14,63 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.odde.massivemailer.controller.SendMailController;
+import com.odde.massivemailer.model.ContactPerson;
 import com.odde.massivemailer.model.Mail;
+import com.odde.massivemailer.service.impl.SqliteContact;
 
 
 public class SendMailControllerTest {
-	
-	
-	
+
 	HttpServletRequest httpReq = Mockito.mock(HttpServletRequest.class);
+	SqliteContact mockedContact = Mockito.mock(SqliteContact.class);
 	SendMailController mailController = new SendMailController();
 	
 	@org.junit.Test
-	public void testProcessRequest(){
+	public void testProcessRequest() throws SQLException{
 		String recipient = "name1@gmail.com;name2@gmail.com";
-		
+
 		Mockito.when(httpReq.getParameter("recipient")).thenReturn(recipient);
 		Mockito.when(httpReq.getParameter("content")).thenReturn("content-na-ka");
 		Mockito.when(httpReq.getParameter("subject")).thenReturn("suject for test");
-		
-		Mail mail = mailController.processRequest(httpReq);
-		
-	    Assert.assertEquals("suject for test",mail.getSubject());
-	    List<String> repList = mail.getReceipts();
-	    Assert.assertEquals("name1@gmail.com",repList.get(0));
-	    Assert.assertEquals("name2@gmail.com",repList.get(1));
-	    Assert.assertEquals("content-na-ka",mail.getContent());
+
+		Mail mail = new Mail();
+		mail = mailController.processRequest(httpReq);
+
+		Assert.assertEquals("suject for test",mail.getSubject());
+		List<String> repList = mail.getReceipts();
+		Assert.assertEquals("name1@gmail.com",repList.get(0));
+		Assert.assertEquals("name2@gmail.com",repList.get(1));
+		Assert.assertEquals("content-na-ka",mail.getContent());
 	}
 	
-	
+
+	@org.junit.Test
+	public void testProcessRequestByCompany() throws SQLException{
+		
+		String recipient = "name1@gmail.com;company:abc";
+
+		Mockito.when(httpReq.getParameter("recipient")).thenReturn(recipient);
+		Mockito.when(httpReq.getParameter("content")).thenReturn("Company-na-ka");
+		Mockito.when(httpReq.getParameter("subject")).thenReturn("suject for test");
+		
+		List<ContactPerson> contactPersonList = new ArrayList<ContactPerson>();
+		contactPersonList.add(new ContactPerson("", "ab1@abc.com", "", "abc"));
+		contactPersonList.add(new ContactPerson("", "ab2@abc.com", "", "abc"));
+		contactPersonList.add(new ContactPerson("", "ab3@abc.com", "", "abc"));
+		Mockito.when(mockedContact.getContactListFromCompany("abc")).thenReturn(contactPersonList);
+		
+		mailController.setSqliteContact(mockedContact);
+		
+		Mail mail = new Mail();
+		mail = mailController.processRequest(httpReq);
+
+		Assert.assertEquals("suject for test",mail.getSubject());
+		Assert.assertEquals("Company-na-ka",mail.getContent());
+		List<String> repList = mail.getReceipts();
+		Assert.assertEquals("name1@gmail.com",repList.get(0));		
+		Assert.assertEquals("ab1@abc.com",repList.get(1));
+		Assert.assertEquals("ab2@abc.com",repList.get(2));
+		Assert.assertEquals("ab3@abc.com",repList.get(3));		
+	}
 	
 }
