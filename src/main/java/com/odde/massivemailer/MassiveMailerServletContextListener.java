264458ef0094ddb6f7a8ba88952315e48d5f1eef
Sng Swee Keat <sng_swee_keat@ida.gov.sg>
2016-09-26 17:46:55 +0800

    added check for environment variables EMAIL_USERID and EMAIL_PASSWORD

--- src/main/java/com/odde/massivemailer/MassiveMailerServletContextListener.java
+++ src/main/java/com/odde/massivemailer/MassiveMailerServletContextListener.java
@@ -5,6 +5,10 @@ import javax.servlet.ServletContextListener;
 
 
 public class MassiveMailerServletContextListener implements ServletContextListener {
+
+    private static final String EMAIL_USERID = "EMAIL_USERID";
+    private static final String EMAIL_PASSWORD = "EMAIL_PASSWORD";
+
     @Override
     public void contextDestroyed(ServletContextEvent arg0) {
         System.out.println("ServletContextListener destroyed");
@@ -14,5 +18,14 @@ public class MassiveMailerServletContextListener implements ServletContextListen
     @Override
     public void contextInitialized(ServletContextEvent arg0) {
         System.out.println("ServletContextListener started");
+        System.out.println("Printing existing environment variables");
+
+        String emailUserID = System.getenv(EMAIL_USERID);
+        String emailPassword = System.getenv(EMAIL_PASSWORD);
+
+        if(null == emailUserID || null == emailPassword){
+            System.out.println("Necessary environment variable does not exist yet!");
+        }
+
     }
 }
