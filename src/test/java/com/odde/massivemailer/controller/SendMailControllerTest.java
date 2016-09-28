2298b2393ce0eeb5c9cf6eda9a9121d2e49dd2e0
Ryan Tan <languantan@gmail.com>
2016-09-28 11:37:45 +0800

    rewrote the test abit

--- src/test/java/com/odde/massivemailer/controller/SendMailControllerTest.java
+++ src/test/java/com/odde/massivemailer/controller/SendMailControllerTest.java
@@ -21,56 +21,55 @@ import com.odde.massivemailer.service.impl.SqliteContact;
 
 public class SendMailControllerTest {
 
-	HttpServletRequest httpReq = Mockito.mock(HttpServletRequest.class);
-	SqliteContact mockedContact = Mockito.mock(SqliteContact.class);
-	SendMailController mailController = new SendMailController();
-	
-	@org.junit.Test
-	public void testProcessRequest() throws SQLException{
-		String recipient = "name1@gmail.com;name2@gmail.com";
+    HttpServletRequest httpReq = Mockito.mock(HttpServletRequest.class);
+    SqliteContact mockedContact = Mockito.mock(SqliteContact.class);
+    SendMailController mailController = new SendMailController();
 
-		Mockito.when(httpReq.getParameter("recipient")).thenReturn(recipient);
-		Mockito.when(httpReq.getParameter("content")).thenReturn("content-na-ka");
-		Mockito.when(httpReq.getParameter("subject")).thenReturn("suject for test");
- 
-		Mail mail = new Mail();
-		mail = mailController.processRequest(httpReq);
+    private String recipient;
 
-		Assert.assertEquals("suject for test",mail.getSubject());
-		List<String> repList = mail.getReceipts();
-		Assert.assertEquals("name1@gmail.com",repList.get(0));
-		Assert.assertEquals("name2@gmail.com",repList.get(1));
-		Assert.assertEquals("content-na-ka",mail.getContent());
-	}
-	
+    @org.junit.Before
+    public void setup() {
+        Mockito.when(httpReq.getParameter("content")).thenReturn("content-na-ka");
+        Mockito.when(httpReq.getParameter("subject")).thenReturn("subject for test");
+    }
 
-	@org.junit.Test
-	public void testProcessRequestByCompany() throws SQLException{
-		
-		String recipient = "name1@gmail.com;company:abc";
+    @org.junit.Test
+    public void testProcessRequest() throws SQLException {
+        recipient = "name1@gmail.com;name2@gmail.com";
+        Mockito.when(httpReq.getParameter("recipient")).thenReturn(recipient);
 
-		Mockito.when(httpReq.getParameter("recipient")).thenReturn(recipient);
-		Mockito.when(httpReq.getParameter("content")).thenReturn("Company-na-ka");
-		Mockito.when(httpReq.getParameter("subject")).thenReturn("suject for test");
-		
-		List<ContactPerson> contactPersonList = new ArrayList<ContactPerson>();
-		contactPersonList.add(new ContactPerson("", "ab1@abc.com", "", "abc"));
-		contactPersonList.add(new ContactPerson("", "ab2@abc.com", "", "abc"));
-		contactPersonList.add(new ContactPerson("", "ab3@abc.com", "", "abc"));
-		Mockito.when(mockedContact.getContactListFromCompany("abc")).thenReturn(contactPersonList);
-		
-		mailController.setSqliteContact(mockedContact);
-		
-		Mail mail = new Mail();
-		mail = mailController.processRequest(httpReq);
+        Mail mail = mailController.processRequest(httpReq);
 
-		Assert.assertEquals("suject for test",mail.getSubject());
-		Assert.assertEquals("Company-na-ka",mail.getContent());
-		List<String> repList = mail.getReceipts();
-		Assert.assertEquals("name1@gmail.com",repList.get(0));		
-		Assert.assertEquals("ab1@abc.com",repList.get(1));
-		Assert.assertEquals("ab2@abc.com",repList.get(2));
-		Assert.assertEquals("ab3@abc.com",repList.get(3));		
-	}
-	
+        Assert.assertEquals("subject for test", mail.getSubject());
+        List<String> repList = mail.getReceipts();
+        Assert.assertEquals("name1@gmail.com", repList.get(0));
+        Assert.assertEquals("name2@gmail.com", repList.get(1));
+        Assert.assertEquals("content-na-ka", mail.getContent());
+    }
+
+
+    @org.junit.Test
+    public void testProcessRequestByCompany() throws SQLException {
+
+        recipient = "company:abc";
+        Mockito.when(httpReq.getParameter("recipient")).thenReturn(recipient);
+
+        String[] companyRecipients = {"ab1@abc.com", "ab2@abc.com", "ab3@abc.com"};
+
+        List<ContactPerson> contactPersonList = new ArrayList<>();
+        for (int i = 0; i < companyRecipients.length; ++i) {
+            contactPersonList.add(new ContactPerson("", companyRecipients[i], "", "abc"));
+        }
+
+        Mockito.when(mockedContact.getContactListFromCompany("abc")).thenReturn(contactPersonList);
+        mailController.setSqliteContact(mockedContact);
+        Mail mail = mailController.processRequest(httpReq);
+
+        List<String> repList = mail.getReceipts();
+        for (int i = 0; i < repList.size(); ++i) {
+            Assert.assertEquals(companyRecipients[i], repList.get(i));
+        }
+
+    }
+
 }
