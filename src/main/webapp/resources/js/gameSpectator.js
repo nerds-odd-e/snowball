102cfd117f3015cf0769fa6ebe8b0d898e5ebbdf
Ken Chuan <yeo_ken_chuan@ida.gov.sg>
2016-09-30 11:30:14 +0800

    Added in missing fun

--- src/main/webapp/resources/js/gameSpectator.js
+++ src/main/webapp/resources/js/gameSpectator.js
@@ -31,32 +31,50 @@ function checkGameState(){
     */
 
     // single player 
-    var playerInfo = getPlayerInfo(); 
-    var playerList = JSON.parse(playerInfo); 
-    updatePlayerCount(playerList.length);  
-    var html = ''; 
-    for (var p in playerList) { 
-        html += updatePlayerPosition(playerList[p]); 
-    } 
+    var playerInfo = getPlayerInfo();
+    var playerList = JSON.parse(playerInfo);
+    updatePlayerCount(playerList.length);
+    var html = '';
+    for (var p in playerList) {
+        html += updatePlayerPosition(playerList[p]);
+    }
     $('#canvas').html(html);
 }
 
 function updatePlayerPosition(player){
     var html = '';
 
-    if(player != undefined || player != null){
-        html = '<div class="race-track">'+ 
-                    '<div class="racer" style="margin-left:'+player.position*10+'px">' + 
-                        '<div id="racerName">ID: '    + player.ID        +
-                    '</div>'+ 
-                    '<div id="racerEmail">Email: '+ player.email     + '</div>'+ 
-                    '<div id="racerDist">Dist: ' + player.position  + '</div>'+ 
-                    '<div id="scar">Scar: '      + player.scars     + '</div>' + 
-                '</div>'+ 
+    if(player != undefined || player != null) {
+        html = '<div class="race-track">'+
+                    '<div class="racer" style="margin-left:'+player.position*10+'px">' +
+                        '<div id="racerName">ID: '    + player.ID+
+                    '</div>'+
+                    '<div id="racerEmail">Email: '+ player.email     + '</div>'+
+                    '<div id="racerDist">Dist: ' + player.position  + '</div>'+
+                    '<div id="scar">Scar: '      + player.scars     + '</div>' +
+                '</div>'+
             '</div>';
     }
 
     return html;
+}
+
+function getPlayerInfo() {
+    var r = "";
+    $.ajax({
+        type:           "GET",
+        async:          false,
+        contentType:    "application/json",
+        url:            "/massive_mailer/emersonsgame/Players",
+        success: function(data) {
+            r = data;
+        },
+        error: function(err) {
+            console.log(err);
+        }
+    });
+
+    return r;
 }
 
 function updateScreenWithPlayerData(results){
