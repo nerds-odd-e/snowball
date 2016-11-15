802eb9a0c2178f7f6e088abdafd33ad52a174f7b
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-15 14:33:30 +0800

    Load migration files and run them

--- src/main/java/com/odde/massivemailer/startup/PrepareDatabaseStartupListener.java
+++ src/main/java/com/odde/massivemailer/startup/PrepareDatabaseStartupListener.java
@@ -4,10 +4,17 @@ import com.odde.massivemailer.service.impl.SqliteBase;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.io.*;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class PrepareDatabaseStartupListener implements ServletContextListener {
+    private static final String[] MIGRATION_FILES = {
+            "20150806140746_Add_firstname_lastname_to_mail_table.sql",
+            "20150906140746_Add_template_table.sql",
+            "20161115132000_create_notifications.sql"
+    };
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("Preparing database...");
@@ -15,9 +22,9 @@ public class PrepareDatabaseStartupListener implements ServletContextListener {
         SqliteBase base = new SqliteBase();
 
         try (Statement statement = base.openConnection()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS mail (id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL, name VARCHAR(50), email VARCHAR(50) NOT NULL, lastname VARCHAR(50), company VARCHAR(50))");
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS Template (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, TemplateName VARCHAR(255) NOT NULL, Subject VARCHAR(255), Content NVARCHAR(5000))");
-            //statement.executeUpdate("INSERT INTO Template (TemplateName,Subject,Content) VALUES ('Default Template 1', 'Greeting {FirstName}', 'Hi, {FirstName} {LastName} from {Company}')");
+            for (String migration : MIGRATION_FILES) {
+                statement.executeUpdate(loadMigration(migration));
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -30,5 +37,28 @@ public class PrepareDatabaseStartupListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
 
+    }
+
+    private String loadMigration(String name) {
+        String path = "/db/migration/" + name;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(path)))) {
+            StringBuilder builder = new StringBuilder();
+            String line = reader.readLine();
+
+            while (line != null) {
+                builder.append(line);
+
+                line = reader.readLine();
+            }
+
+            return builder.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
