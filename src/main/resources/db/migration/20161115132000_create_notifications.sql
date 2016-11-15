1c0d2f034f2b3daafda35ce42c6d0ee8148a66ab
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-15 13:33:21 +0800

    Only create notification tables if they do not exist

--- src/main/resources/db/migration/20161115132000_create_notifications.sql
+++ src/main/resources/db/migration/20161115132000_create_notifications.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS notifications;
 DROP TABLE IF EXISTS notification_details;
 
-CREATE TABLE notifications (
+CREATE TABLE IF NOT EXISTS notifications (
     id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     template_id INTEGER NOT NULL,
     sent_at     TIMESTAMP
 );
 
-CREATE TABLE notification_details (
+CREATE TABLE IF NOT EXISTS notification_details (
     id              INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     notification_id INTEGER NOT NULL,
     contact_id      INTEGER NOT NULL,
