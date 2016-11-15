f668d8e114399df8d7fbf883233ec744f2968889
sangkyoonnam <sangkyoon.nam@namsang.org>
2016-11-15 13:37:24 +0800

    fixed error again

--- src/test/java/gradle/cucumber/EmailTrackingStepdefs.java
+++ src/test/java/gradle/cucumber/EmailTrackingStepdefs.java
@@ -11,19 +11,19 @@ public class EmailTrackingStepdefs {
     private WebDriverWrapper driver = WebDriverFactory.getDefaultDriver();
     private String BASE_URL = "http://localhost:8070/massive_mailer/";
 
-    public String mSubject = null;
+//    public String mSubject = null;
 
     @Given("^I sent an email and store \"([^\"]*)\"$")
     public void sendEmailAndStore(String subject) throws Throwable {
 //        driver.text_field("recipient", recipient);
 //        driver.findElementById("subject1").
-        mSubject = subject;
+//        mSubject = subject;
     }
 
     @When("^Visit Email Tracking Page$")
     public void visitSendMailPage() throws Throwable {
         driver.visit(BASE_URL + "email_tracking.jsp");
-        driver.findElementById("subject1").
+//        driver.findElementById("subject1")
     }
 
     @Then("^I should get an element \"([^\"]*)\" and \"([^\"]*)\"$")
