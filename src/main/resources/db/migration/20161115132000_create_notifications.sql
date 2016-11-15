01f0bd3d329f693028cc2db69fbc33faa4c61a54
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-15 13:25:12 +0800

    Initial notifications and notification_details tables

---
+++ src/main/resources/db/migration/20161115132000_create_notifications.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS notifications;
+DROP TABLE IF EXISTS notification_details;
+
+CREATE TABLE notifications (
+    id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    template_id INTEGER NOT NULL,
+    sent_at     TIMESTAMP
+);
+
+CREATE TABLE notification_details (
+    id              INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    notification_id INTEGER NOT NULL,
+    contact_id      INTEGER NOT NULL,
+    mail_read_count INTEGER NOT NULL DEFAULT 0
+);
