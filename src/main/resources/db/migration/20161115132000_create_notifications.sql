033287cdd9fee1076ffec67aee45c333fe7a6f82
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-15 13:41:19 +0800

    Add FOREIGN KEY constrants to notification tables

--- src/main/resources/db/migration/20161115132000_create_notifications.sql
+++ src/main/resources/db/migration/20161115132000_create_notifications.sql
@@ -4,12 +4,17 @@ DROP TABLE IF EXISTS notification_details;
 CREATE TABLE IF NOT EXISTS notifications (
     id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     template_id INTEGER NOT NULL,
-    sent_at     TIMESTAMP
+    sent_at     TIMESTAMP,
+
+    FOREIGN KEY (template_id)   REFERENCES Template(Id)
 );
 
 CREATE TABLE IF NOT EXISTS notification_details (
     id              INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     notification_id INTEGER NOT NULL,
     contact_id      INTEGER NOT NULL,
-    mail_read_count INTEGER NOT NULL DEFAULT 0
+    mail_read_count INTEGER NOT NULL DEFAULT 0,
+
+    FOREIGN KEY (notification_id)   REFERENCES notifications(id),
+    FOREIGN KEY (contact_id)        REFERENCES mail(id)
 );
