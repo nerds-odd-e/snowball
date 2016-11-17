ec164a46f010703a702c23b0047a0e86e29a0153
sangkyoonnam <sangkyoon.nam@namsang.org>
2016-11-17 14:47:34 +0800

    adding changes for test cases enhancement

--- src/main/webapp/resources/js/showEmailDetails.js
+++ src/main/webapp/resources/js/showEmailDetails.js
@@ -15,18 +15,38 @@ function retrieveEmailDetailsListFromServer()
 
 function renderEmailDetailsSummary(json, selector)
 {
-    var item = json;
-    var subject = item.subject===undefined?'':item.subject;
-	var sent_at = item.sent_at===undefined?'':item.sent_at;
-	var total_open_count = item.total_open_count===undefined?'':item.total_open_count;
-	selector.append('<tr><td class="subject">'+subject+'</td><td class="sent_at">'+sent_at+'</td><td class="total_open_count">'+total_open_count+'</td></tr></a>');
+	selector.append(new NotificationSummary(json).createRow());
+}
+
+
+function NotificationSummary(item)
+{
+    this.getString = function(attr) {
+        return attr===undefined?'':attr;
+    };
+    this.createRow = function() {
+        return '<tr><td class="subject">'+this.subject+'</td><td class="sent_at">'+this.sent_at+'</td><td class="total_open_count">'+this.total_open_count+'</td></tr></a>'
+    }
+    this.subject=this.getString(item.subject);
+    this.sent_at=this.getString(item.sent_at);
+    this.total_open_count=this.getString(item.total_open_count);
 }
 
 function renderEmailDetailsList(json, selector)
 {
 	$.each(json.emails, function(idx, item) {
-		var email = item.email===undefined?'':item.email;
-		var open_count = item.open_count===undefined?'':item.open_count;
-		selector.append('<tr"><td class="email">'+email+'</td><td class="open_count">'+open_count+'</td></tr></a>');
+        selector.append(new NotificationDetail(item).createRow());
 	})
+}
+
+function NotificationDetail(item)
+{
+    this.getString = function(attr) {
+        return attr===undefined?'':attr;
+    };
+    this.createRow = function() {
+        return '<tr"><td class="email">'+this.email+'</td><td class="open_count">'+this.open_count+'</td></tr></a>';
+    }
+    this.email=this.getString(item.email);
+    this.open_count=this.getString(item.open_count);
 }
