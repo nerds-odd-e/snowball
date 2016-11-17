5d1720864d8a856e001603d87ea523a67dc8d0dd
sangkyoonnam <sangkyoon.nam@namsang.org>
2016-11-17 14:00:37 +0800

    adding changes for test cases enhancement

--- src/test/javascript/showTrackingEmailTest.js
+++ src/test/javascript/showTrackingEmailTest.js
@@ -2,7 +2,8 @@ describe('test show tracking function', function() {
 
 	var rootId = "testContainer";
 	var markup = "<table id='trackingTable'><tbody></tbody></table>";
-	var mock_json = [{"notificationId":1,"subject":"test","sentDate":"2016-11-14"},{"notificationId":2,"subject":"test2","sentDate":"2016-11-14"}];
+	var firstNotification = {"notificationId":1,"subject":"test","sentDate":"2016-11-14"};
+	var mock_json = [ firstNotification ,{"notificationId":2,"subject":"test2","sentDate":"2016-11-14"}];
 
 	beforeEach(function(){
 		var container = document.createElement('div');
@@ -23,6 +24,22 @@ describe('test show tracking function', function() {
 		    expect($("#trackingTable .subject").eq(idx).text()).toBe(item.subject);
             expect($("#trackingTable .sentDate").eq(idx).text()).toBe(item.sentDate);
 		});
+	});
+
+	it('should render empty string for missing attributes',function() {
+	    delete firstNotification.subject;
+		renderTrackingEmailList(mock_json, $("#trackingTable tbody"));
+
+        expect($("#trackingTable .subject").eq(0).text()).toBe("");
+        expect($("#trackingTable .sentDate").eq(0).text()).toBe(mock_json[0].sentDate);
+	});
+
+	describe("Notifiction", function() {
+	    it('should return corret table row', function() {
+	        var row;
+	        row = new Notification(firstNotification).createRow();
+	        expect(row.indexOf("notification_id="+firstNotification.notificationId) > 0).toBe(true);
+	    });
 	});
 
 });
