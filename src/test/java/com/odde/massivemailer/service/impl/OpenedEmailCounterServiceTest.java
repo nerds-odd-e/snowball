8344e4fa91a344aa81f65a306da4e543aed90d98
sangkyoonnam <sangkyoon.nam@namsang.org>
2016-11-18 13:02:49 +0800

    solve conflict

--- src/test/java/com/odde/massivemailer/service/impl/OpenedEmailCounterServiceTest.java
+++ src/test/java/com/odde/massivemailer/service/impl/OpenedEmailCounterServiceTest.java
@@ -9,6 +9,9 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
@@ -20,39 +23,44 @@ public class OpenedEmailCounterServiceTest {
     EmailCounterTracking emailCounterTracking = new EmailCounterTracking();
     NotificationService notificationService = new NotificationServiceSqlite();
 
-
     @Before
     public void setUp() {
         service.destroyAll();
     }
 
-    @Test @Ignore
+    @Test
     public void shouldReturnEmptyJasonWhenNobodyOpenedTheEmail() {
-        assertEquals("[]", service.getEmailCounterJson(email_id));
+        assertEquals("{\"subject\":\"null\", \"sent_at\":\"null\", \"total_open_count\":0, \"emails\":[]}", service.getEmailCounterJson(email_id));
     }
 
-    @Test @Ignore
+    @Test
     public void shouldReturnRecordWithCountWhenOnePersonOpenedTheEmail4Times() {
         Notification notification = createNotification();
         addRecipient(notification, "terry@odd-e.com");
         Notification savedNotification = notificationService.save(notification);
-        assertEquals("[{'email': 'terry@odd-e.com'; 'count': 4}]", service.getEmailCounterJson(savedNotification.getNotificationId()));
+        assertEquals("{\"subject\":\"test subject\", \"sent_at\":\"Fri Nov 18 03:19:03 SGT 2016\", \"total_open_count\":4, \"emails\":[{\"email\": \"terry@odd-e.com\", \"open_count\": 4}]}", service.getEmailCounterJson(savedNotification.getNotificationId()));
     }
 
-    @Test @Ignore
+    @Test
     public void shouldReturnRecordWithCountWhenTwoPersonOpenedTheEmail() {
         Notification notification = createNotification();
         addRecipient(notification, "terry@odd-e.com");
         addRecipient(notification, "trump@odd-e.com");
         Notification savedNotification = notificationService.save(notification);
-        assertEquals("[{'email': 'terry@odd-e.com'; 'count': 4}, {'email': 'trump@odd-e.com'; 'count': 4}]", service.getEmailCounterJson(savedNotification.getNotificationId()));
+        assertEquals("{\"subject\":\"test subject\", \"sent_at\":\"Fri Nov 18 03:19:03 SGT 2016\", \"total_open_count\":8, \"emails\":[{\"email\": \"terry@odd-e.com\", \"open_count\": 4}, {\"email\": \"trump@odd-e.com\", \"open_count\": 4}]}", service.getEmailCounterJson(savedNotification.getNotificationId()));
     }
 
     private Notification createNotification() {
         Notification notification = new Notification();
         notification.setNotificationId(email_id);
         notification.setSubject("test subject");
-        notification.setSentDate(new Date());
+        String input = "Fri Nov 18 03:19:03 SGT 2016";
+        SimpleDateFormat parser = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy");
+        try {
+            notification.setSentDate(parser.parse(input));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         return notification;
     }
 
