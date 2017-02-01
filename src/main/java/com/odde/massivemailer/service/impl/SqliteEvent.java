f062687d6c4e839fed2ff1640ad0abe3e51950cc
nacamura <t.nacamura@gmail.com>
2017-02-01 09:31:30 +0800

    changed return value type of addNewEvent to boolean

--- src/main/java/com/odde/massivemailer/service/impl/SqliteEvent.java
+++ src/main/java/com/odde/massivemailer/service/impl/SqliteEvent.java
@@ -4,37 +4,25 @@ import com.odde.massivemailer.model.Event;
 
 import java.sql.SQLException;
 
-/**
- * Created by csd11 on 31/1/17.
- */
 public class SqliteEvent extends SqliteBase {
-    public int addNewEvent(com.odde.massivemailer.model.Event event) {
+    public boolean addNewEvent(Event event) {
         int rowAffected = 0;
 
         try {
             openConnection();
+
             String sqlStatement =
                     String.format("INSERT INTO Event (Title, Description) VALUES ('%s', '%s')",
                         event.getTitle(),
                         event.getContent());
             rowAffected = statement.executeUpdate(sqlStatement);
+
+            return (rowAffected == 1);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+            return false;
         } finally {
             closeConnection();
         }
-        return rowAffected;
     }
-
-/*
-    private int saveContactToDatabase(String name, String email, String lastname, String company)
-            throws SQLException {
-        int rowAffected = 0;
-        if (!contactExisted(email))
-            rowAffected = statement
-                    .executeUpdate("INSERT INTO mail(name,email,lastname,company) VALUES ('"
-                            + name + "', '" + email + "','" + lastname + "','" + company + "')");
-        return rowAffected;
-    }
-    */
 }
