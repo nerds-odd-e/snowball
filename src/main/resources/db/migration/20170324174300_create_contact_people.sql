8e040847db19b266de525b9f0d3710c5dc79201b
carrie <autumnleaves1459@gmail.com>
2017-03-28 14:35:36 +0800

    modify database tables to include location

--- src/main/resources/db/migration/20170324174300_create_contact_people.sql
+++ src/main/resources/db/migration/20170324174300_create_contact_people.sql
@@ -1,8 +1,10 @@
+DROP TABLE IF EXISTS contact_people;
 CREATE TABLE IF NOT EXISTS contact_people (
     id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name        VARCHAR(50),
     email       VARCHAR(50),
     firstname    VARCHAR(50),
     lastname    VARCHAR(50),
-    company     VARCHAR(50)
+    company     VARCHAR(50),
+    location    VARCHAR(255) NOT NULL DEFAULT ''
 );
