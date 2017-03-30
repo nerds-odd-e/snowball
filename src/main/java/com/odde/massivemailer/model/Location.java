6205906c27c578d0de2d458d824649c81f8c6790
carrie <autumnleaves1459@gmail.com>
2017-03-30 17:24:01 +0800

    refactor distanceFrom method in Location class

--- src/main/java/com/odde/massivemailer/model/Location.java
+++ src/main/java/com/odde/massivemailer/model/Location.java
@@ -21,11 +21,11 @@ public class Location {
         double differenceOfLongInRadian = toRadian(location.lng - lng);
         // a is the square of half the chord length between the points.
         double a = Math.sin(differenceOfLatInRadian / 2) * Math.sin(differenceOfLatInRadian / 2) +
-                Math.cos(toRadian(location.lat)) * Math.cos(toRadian(lat)) *
-                        Math.sin(differenceOfLongInRadian / 2) * Math.sin(differenceOfLongInRadian / 2);
+                   Math.cos(toRadian(location.lat)) * Math.cos(toRadian(lat)) *
+                   Math.sin(differenceOfLongInRadian / 2) * Math.sin(differenceOfLongInRadian / 2);
         double angularDistance = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        Double distance = earthRadiusInKm * angularDistance;
-        return distance.intValue(); // returns the distance in kilometers
+        Double distanceInKm = earthRadiusInKm * angularDistance;
+        return distanceInKm.intValue();
     };
 
     public String getName() {
