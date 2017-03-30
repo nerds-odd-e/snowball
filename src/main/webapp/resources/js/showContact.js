afeb1db4da28d2974626e0525a6d052eed060d69
jack_jiebo <dyihoon90@gmail.com>
2017-03-30 15:51:27 +0800

    code refactoring

--- src/main/webapp/resources/js/showContact.js
+++ src/main/webapp/resources/js/showContact.js
@@ -13,36 +13,40 @@ function retrieveContactListFromServer()
 	return contactList;
 }
 
+function Contact(attributes) {
+    this.firstName = attributes.firstname===undefined?'':attributes.firstname;
+    this.lastName = attributes.lastname===undefined?'':attributes.lastname;
+    this.company = attributes.company===undefined?'':attributes.company;
+    this.location = attributes.location===undefined?'':attributes.location;
+    this.email = attributes.email===undefined?'':attributes.email;
+}
+
 function renderContactList(json, selector)
 {
 	$.each(json, function(idx, item) {
-		var firstName = item.attributes.firstname===undefined?'':item.attributes.firstname;
-		var lastName = item.attributes.lastname===undefined?'':item.attributes.lastname;
-		var company = item.attributes.company===undefined?'':item.attributes.company;
-		var location = item.attributes.location===undefined?'':item.attributes.location;
-		selector.append('<li class="col-md-2 email-address" style="text-align: left">'+item.attributes.email+'</li>');
-		selector.append('<li class="col-md-2" style="text-align: left">'+firstName+'</li>');
-		selector.append('<li class="col-md-2" style="text-align: left">'+lastName+'</li>');
-		selector.append('<li class="col-md-2 company" style="text-align: left">'+company+'</li>');
-		selector.append('<li class="col-md-2 location" style="text-align: left">'+location+'</li>');
+        var contact = new Contact(item.attributes);
+
+		selector.append('<li class="col-md-2 email-address" style="text-align: left">'+contact.email+'</li>');
+		selector.append('<li class="col-md-2" style="text-align: left">'+contact.firstName+'</li>');
+		selector.append('<li class="col-md-2" style="text-align: left">'+contact.lastName+'</li>');
+		selector.append('<li class="col-md-2 company" style="text-align: left">'+contact.company+'</li>');
+		selector.append('<li class="col-md-2 location" style="text-align: left">'+contact.location+'</li>');
 		selector.append('<li class="col-md-1" style="text-align: left; padding-bottom: 1%"><input id=\'edit_button\' type=\'button\' name=\'edit\' value=\'edit\' onclick=\'showEditContactDetail('+JSON.stringify(item)+')\'/></li>');
-	})
+    })
 }
 
 function renderContactSelectionList(json, selector)
 {
 	selector.html('');
 	$.each(json, function(idx, item) {
-		var firstName = item.attributes.firstname===undefined?'':item.attributes.firstname;
-		var lastName = item.attributes.lastname===undefined?'':item.attributes.lastname;
-		var company = item.attributes.company===undefined?'':item.attributes.company;
-		var location = item.attributes.location===undefined?'':item.attributes.location;
+		var contact = new Contact(item.attributes);
+
 		selector.append('<li class="col-md-1 email-checkbox" style="text-align: center"><input type="checkbox" onclick="whenContactIsSelected(' + idx + ')" id="' + idx + '" value="' + item.attributes.email + '" /></li>');
-		selector.append('<li class="col-md-3 email-address" style="text-align: left">'+item.attributes.email+'</li>');
-		selector.append('<li class="col-md-2 contact-name" style="text-align: left">'+firstName+'</li>');
-		selector.append('<li class="col-md-3 contact-lname" style="text-align: left">'+lastName+'</li>');
-		selector.append('<li class="col-md-3 contact-cname" style="text-align: left">'+company+'</li>');
-		selector.append('<li class="col-md-3 contact-location" style="text-align: left">'+location+'</li>');
+		selector.append('<li class="col-md-3 email-address" style="text-align: left">'+contact.email+'</li>');
+		selector.append('<li class="col-md-2 contact-name" style="text-align: left">'+contact.firstName+'</li>');
+		selector.append('<li class="col-md-3 contact-lname" style="text-align: left">'+contact.lastName+'</li>');
+		selector.append('<li class="col-md-3 contact-cname" style="text-align: left">'+contact.company+'</li>');
+		selector.append('<li class="col-md-3 contact-location" style="text-align: left">'+contact.location+'</li>');
 	})
 }
 
@@ -78,3 +82,4 @@ function openEditContactModal()
 function submitEditContact() {
 	$("#editContact").submit();
 }
+
