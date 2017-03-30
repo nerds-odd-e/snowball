87e886d1ecce3bd1e81ac7a0a62a8febcbb1be26
carrie <autumnleaves1459@gmail.com>
2017-03-30 10:12:25 +0800

    revert changes for notification migration files

--- src/main/resources/db/migration/20161115132000_create_notifications.sql
+++ src/main/resources/db/migration/20161115132000_create_notifications.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS notifications (
     id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     template_id INTEGER NOT NULL,
-    subject         VARCHAR(255) NOT NULL DEFAULT '',
     sent_at     TIMESTAMP,
-    notification_id INTEGER NOT NULL DEFAULT 0
 
+    FOREIGN KEY (template_id)   REFERENCES Template(Id)
 );
 
 CREATE TABLE IF NOT EXISTS notification_details (
     id              INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     notification_id INTEGER NOT NULL,
-    email_address   VARCHAR(255) NOT NULL,
-    read_count      INTEGER NOT NULL DEFAULT 0,
+    contact_id      INTEGER NOT NULL,
+    mail_read_count INTEGER NOT NULL DEFAULT 0,
 
-    FOREIGN KEY (notification_id)   REFERENCES notifications(id)
+    FOREIGN KEY (notification_id)   REFERENCES notifications(id),
+    FOREIGN KEY (contact_id)        REFERENCES mail(id)
 );
---
+++ src/main/resources/db/migration/20161115175300_add_notification_id_to_notifications.sql
@@ -0,0 +1,1 @@
+ALTER TABLE notifications ADD COLUMN notification_id INTEGER NOT NULL DEFAULT 0;
