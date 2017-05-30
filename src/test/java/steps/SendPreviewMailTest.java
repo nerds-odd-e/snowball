d451c5039d2e71638a8aeacd6fde9c0403c0007e
sujitkumar7 <sujitkumar.symbi@gmail.com>
2017-05-30 17:29:52 +0800

    MAil Address

--- src/test/java/steps/SendPreviewMailTest.java
+++ src/test/java/steps/SendPreviewMailTest.java
@@ -1,15 +1,43 @@
 package steps;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import steps.driver.WebDriverFactory;
 import steps.driver.WebDriverWrapper;
 
-/**
- * Created by csd on 30/5/17.
- */
-public class SendPreviewMailTest {
-    private static final String BASE_URL = "http://localhost:8070/massive_mailer/";
 
-    private WebDriverWrapper driver = WebDriverFactory.getDefaultDriver();
+public class SendPreviewMailTest {
+    @Given("^there is a course starting from \"([^\"]*)\"$")
+    public void there_is_a_course_starting_from(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^there are students with email  loaded for this course$")
+    public void there_are_students_with_email_loaded_for_this_course() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^i send preview email for this course$")
+    public void i_send_preview_email_for_this_course() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^admin@odd-e\\.com should receive the preview email$")
+    public void admin_odd_e_com_should_receive_the_preview_email() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^there are students with email student@odd-e\\.com loaded for this course$")
+    public void there_are_students_with_email_student_odd_e_com_loaded_for_this_course() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 
 
 }
