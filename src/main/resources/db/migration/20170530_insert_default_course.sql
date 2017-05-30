74b5aa4a7e303dcc51e7fa9c1739005119103025
swapna942 <swapna942@gmail.com>
2017-05-30 14:51:49 +0800

    Course table default course insert script

---
+++ src/main/resources/db/migration/20170530_insert_default_course.sql
@@ -0,0 +1,2 @@
+INSERT INTO courses (id, coursename, duration,location,startdate,address,coursedetails,instructor)
+VALUES ('101','CSD', '30', 'Singapore','16-JUL-2017','odd-e','CSD Training','Terry');
