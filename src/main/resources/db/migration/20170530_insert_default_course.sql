c70dee95f92163e184238c81a90f1e2e3830b010
tosamik <tosamik@gmail.com>
2017-05-30 15:17:31 +0800

    removed hard coded primary key from insert script for courses

--- src/main/resources/db/migration/20170530_insert_default_course.sql
+++ src/main/resources/db/migration/20170530_insert_default_course.sql
@@ -1,2 +1,2 @@
-INSERT INTO courses (id, coursename, duration,location,startdate,address,coursedetails,instructor)
-VALUES ('101','CSD', '30', 'Singapore','16-JUL-2017','odd-e','CSD Training','Terry');
+INSERT INTO courses (coursename, duration,location,startdate,address,coursedetails,instructor)
+VALUES ('CSD', '30', 'Singapore','16-JUL-2017','odd-e','CSD Training','Terry');
