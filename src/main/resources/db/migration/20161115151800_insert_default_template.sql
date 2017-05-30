c0dbc5e0bbe288e3f1ffaff8aedfda417432d86e
csd <csd@Q.local>
2017-05-30 16:28:31 +0800

    updated migration DB script to update template

--- src/main/resources/db/migration/20161115151800_insert_default_template.sql
+++ src/main/resources/db/migration/20161115151800_insert_default_template.sql
@@ -9,3 +9,16 @@ VALUES ('Default Template 1', 'Greeting {FirstName}', 'Hi {FirstName} {LastName}
 
  Best Regards
  {Instructor}');
+
+Update template set "Content" ="Hi {FirstName} {LastName},
+
+ Please find your course details below
+
+ Course: {CourseName}
+ Instructor: {Instructor}
+ Location: {Location}
+
+ Best Regards
+ {Instructor}
+
+ " where templateName='Default Template 1';
