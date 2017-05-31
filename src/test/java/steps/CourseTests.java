20ba09377f472c9b832bcf63f2e523a756c4b315
swapna942 <swapna942@gmail.com>
2017-05-31 12:30:20 +0800

    Restored feature and test files

--- src/test/java/steps/CourseTests.java
+++ src/test/java/steps/CourseTests.java
@@ -1,11 +1,12 @@
 package steps;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import steps.driver.WebDriverFactory;
 import steps.driver.WebDriverWrapper;
-import java.util.Date;
+import java.util.List;
 
 public class CourseTests {
 
@@ -13,28 +14,35 @@ public class CourseTests {
     private String BASE_URL = "http://localhost:8070/massive_mailer/add_event.jsp";
 
     @Given("^I am on create new course page$")
-    public void visitAddEventPage() throws Throwable {
+    public void visitCreateCoursePage() throws Throwable {
+        System.out.println("Test Add Event");
         driver.visit(BASE_URL);
     }
+
+
     @When("^Add a course with below details$")
-    public void addCourseWithGivenDetails(String coursename,String duration,String location,Date startdate,String address,String coursedetails,String instructor) throws Throwable {
-        driver.setTextField("coursename", coursename);
-        driver.setTextField("duration", duration);
-        driver.setTextField("startdate", startdate.toString());
-        driver.setTextField("address", address);
-        driver.setTextField("coursedetails", coursedetails);
-        driver.setTextField("instructor", instructor);
-        driver.setDropdownValue("location", location);
+    public void addCourseWithGivenDetails(DataTable dtCourseDetails) throws Throwable {
+        List<List<String>> courses = dtCourseDetails.raw();
+        courses = courses.subList(1, courses.size());
+        for(List<String> val:courses){
+            driver.setTextField("coursename", val.get(0));
+            driver.setTextField("duration", val.get(1));
+            driver.setDropdownValue("location", val.get(2));
+            driver.setTextField("startdate", val.get(3));
+            driver.setTextField("address", val.get(4));
+            driver.setTextField("coursedetails", val.get(5));
+            driver.setTextField("instructor", val.get(6));
+        }
     }
 
 
     @When("^I click the Create button$")
-    public void clickRegisterEvent() throws Throwable {
+    public void clickSaveEvent() throws Throwable {
         driver.clickButton("save_button");
     }
 
     @Then("^Course should save and successfully saved message should appear$")
-    public void eventListPageShouldContain(String eventTitle,String eventLocation) throws Throwable {
+    public void eventListPageShouldContain() throws Throwable {
         driver.expectRedirect(BASE_URL);
     }
 }
