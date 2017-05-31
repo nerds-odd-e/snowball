64212d30cdf297f5219ab2e29dd359e58df04096
sujitkumar7 <sujitkumar.symbi@gmail.com>
2017-05-31 10:46:56 +0800

    MAil Address

--- src/test/java/steps/SendPreviewMailTest.java
+++ src/test/java/steps/SendPreviewMailTest.java
@@ -4,21 +4,33 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import com.odde.massivemailer.model.ContactPerson;
+import org.junit.Assert;
 import steps.driver.WebDriverFactory;
 import steps.driver.WebDriverWrapper;
 
 
 public class SendPreviewMailTest {
+
+   /* private WebDriverWrapper driver = WebDriverFactory.getDefaultDriver();
+    private String adminEmail, studentEmail;*/
+
+
+
     @Given("^there is a course starting from \"([^\"]*)\"$")
     public void there_is_a_course_starting_from(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+
     }
 
     @Given("^there are students with email  loaded for this course$")
     public void there_are_students_with_email_loaded_for_this_course() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        ContactPerson contact = new ContactPerson("john","student@odd-e.com","kumar","","Singapore");
+        contact.save();
+
+            Assert.assertNull(contact.getEmail());
+
+
     }
 
     @When("^i send preview email for this course$")
