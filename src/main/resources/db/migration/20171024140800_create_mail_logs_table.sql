c4a98bed9d9963a48990c1d2569bbc7196141405
Ato Araki <ato.araki@gmail.com>
2017-10-24 15:59:13 +0900

    Raname sent_at to created_at on MailLog

--- src/main/resources/db/migration/20171024140800_create_mail_logs_table.sql
+++ src/main/resources/db/migration/20171024140800_create_mail_logs_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS mail_logs (
     id                INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     contact_person_id INTEGER NOT NULL,
     course_id         INTEGER NOT NULL,
-    sent_at           TIMESTAMP
+    created_at        TIMESTAMP
 );
