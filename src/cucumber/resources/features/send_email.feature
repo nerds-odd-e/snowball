7cf07e08afe24f8d7749fa5e9cbcc1f3875adcd4
csd <myodde@gmail.com>
2017-10-24 14:59:39 +0800

    send mail cucumber

--- src/cucumber/resources/features/send_email.feature
+++ src/cucumber/resources/features/send_email.feature
@@ -16,4 +16,33 @@ Feature: Send Email
       | gadget@mailinator.com                          | success : Email successfully sent |
       | inspector@mailinator.com;gadget@mailinator.com | success : Email successfully sent |
 
+  Scenario: send email only one event
+    Given There is a contact "abc@odd-e.com" at Tokyo
+    And there are two courses at Tokyo
+    When I trigger the sending twice
+    Then I should get an element with message "abc@odd-e.com is already sent"
+
+  Scenario: add new contact
+    Given There is a contact "abc@odd-e.com" at Tokyo
+    And there are two courses at Tokyo
+    And I trigger the sending once
+    And add contact "xyz@odd-e.com" at Tokyo
+    When I trigger the sending once
+    Then I should get an element with message "Email successfully sent"
+    But "abc@odd-e.com" shouldn't receive
+
+  Scenario: Even though there are multiple courses, one user receive only 1 email
+    Given: there is a contact "abc@odd-e.com" at Tokyo, Seoul
+    And: there are two courses at Tokyo, Souel
+    When: I trigger the sending twice
+    Then: contact "abc@odd-e.com" should receive only 1 email
+
+  Scenario: Report Page Includes
+    Given There is a contact "abc@odd-e.com" at Tokyo
+    And there is a course at Tokyo
+    And I trigger the sending once
+    When Report page Includes
+      | email         | course | SendDate |
+      | abc@odd-e.com | 1      | *        |
+
 
--- src/test/java/steps/SendPreviewMailTest.java
+++ src/test/java/steps/SendPreviewMailTest.java
@@ -2,6 +2,7 @@ package steps;
 
 import com.odde.massivemailer.model.Course;
 import com.odde.massivemailer.model.Participant;
+import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -13,14 +14,21 @@ import steps.driver.WebDriverWrapper;
 import steps.page.CourseListPage;
 import steps.page.EnrollParticipantPage;
 
+import java.time.LocalDateTime;
 
 public class SendPreviewMailTest {
     public static final String A_COURSE = "A course";
+    private static final String BASE_URL = "http://localhost:8070/massive_mailer/";
+    private WebDriverWrapper driver = WebDriverFactory.getDefaultDriver();
 
    /* private WebDriverWrapper driver = WebDriverFactory.getDefaultDriver();
     private String adminEmail, studentEmail;*/
-
-
+    private void addCoursesAtTokyo(int offsetDate) throws Throwable {
+       LocalDateTime d = LocalDateTime.now();
+       d.plusMonths(offsetDate);
+       EventTests eventTests = new EventTests();
+       eventTests.addCourse(A_COURSE, "Tokyo", d.toLocalDate().toString());
+    }
 
     @Given("^there is a course starting from \"([^\"]*)\"$")
     public void there_is_a_course_starting_from(String date) throws Throwable {
@@ -45,6 +53,56 @@ public class SendPreviewMailTest {
     @When("^i send precourse email for this course$")
     public void i_send_precourse_email_for_this_course() throws Throwable {
         new CourseListPage().sendPrecourseEmailFor(A_COURSE);
+    }
+
+    @Given("^There is a contact \"([^\"]*)\" at Tokyo$")
+    public void there_is_a_contact_at_Tokyo(String arg1) throws Throwable {
+        ContactPerson person = new ContactPerson("hoge", arg1, "fuga");
+        person.setLocation("Tokyo");
+        person.save();
+    }
+
+    @Given("^there are two courses at Tokyo$")
+    public void there_are_two_courses_at_Tokyo() throws Throwable {
+        addCoursesAtTokyo(1);
+        addCoursesAtTokyo(30);
+    }
+
+    @When("^I trigger the sending twice$")
+    public void i_trigger_the_sending_twice() throws Throwable {
+        i_trigger_the_sending_once();
+        driver.clickButton("send_button");
+    }
+
+    @Given("^add contact \"([^\"]*)\" at Tokyo$")
+    public void add_contact_at_Tokyo(String arg1) throws Throwable {
+        there_is_a_contact_at_Tokyo(arg1);
+    }
+
+    @When("^I trigger the sending once$")
+    public void i_trigger_the_sending_once() throws Throwable {
+        driver.visit(BASE_URL);
+        driver.clickButton("send_button");
+    }
+
+    @Then("^\"([^\"]*)\" shouldn't receive$")
+    public void shouldn_t_receive(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^there is a course at Tokyo$")
+    public void there_is_a_course_at_Tokyo() throws Throwable {
+        addCoursesAtTokyo(1);
+    }
+
+    @When("^Report page Includes$")
+    public void report_page_Includes(DataTable arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+        throw new PendingException();
     }
 
 }
