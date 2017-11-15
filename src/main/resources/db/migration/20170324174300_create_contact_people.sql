45127da3f8913b11da115d05484616ddc8130bc3
yhsampei <hsampei@yahoo-corp.jp>
2017-11-15 11:34:19 +0900

    2. update contact people table with courses_sent and date_sent

--- src/main/resources/db/migration/20170324174300_create_contact_people.sql
+++ src/main/resources/db/migration/20170324174300_create_contact_people.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS contact_people (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name        VARCHAR(50),
-    email       VARCHAR(50),
-    firstname    VARCHAR(50),
-    lastname    VARCHAR(50),
-    company     VARCHAR(50),
-    location    VARCHAR(255) NOT NULL DEFAULT ''
+    id              INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name            VARCHAR(50),
+    email           VARCHAR(50),
+    firstname       VARCHAR(50),
+    lastname        VARCHAR(50),
+    company         VARCHAR(50),
+    location        VARCHAR(255) NOT NULL DEFAULT '',
+    courses_sent    VARCHAR(50),
+    date_sent       DATETIME
 );
