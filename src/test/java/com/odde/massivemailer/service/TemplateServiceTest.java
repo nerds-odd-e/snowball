d3e917be61d467dcd5a20d97ae9c8959962f1a12
csd <csd@Q.local>
2018-06-19 16:45:44 +0800

    returning default template when no data is provided

--- src/test/java/com/odde/massivemailer/service/TemplateServiceTest.java
+++ src/test/java/com/odde/massivemailer/service/TemplateServiceTest.java
@@ -1,5 +1,16 @@
-import static org.junit.Assert.*;
+package com.odde.massivemailer.service;
+
+import com.odde.massivemailer.template.TemplateProvider;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class TemplateServiceTest {
 
+    @Test()
+    public void applyTemplate() throws Exception{
+        TemplateService templateService = new TemplateService();
+        String resolvedTemplate  = templateService.applyTemplate(TemplateProvider.GDPR_TEMPLATE,null);
+        assertTrue("Template should not be empty" , resolvedTemplate.length() > 1);
+    }
 }
