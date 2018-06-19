095c1df5bd419eb1fb617d2d85f12d3193d54517
yhsampei <hsampei@yahoo-corp.jp>
2018-06-19 15:16:34 +0800

    Template service empty impl

---
+++ src/main/java/com/odde/massivemailer/service/TemplateService.java
@@ -0,0 +1,10 @@
+package com.odde.massivemailer.service;
+
+import java.util.Map;
+
+public class TemplateService {
+    public String applyTemplate(String templateName, Map<String, Object> parameters) {
+        //TODO: implement
+        return null;
+    }
+}
