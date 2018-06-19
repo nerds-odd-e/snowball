c0c826a47a4cd344a5838891b9447764629dce0e
Raghu <torajakumar@gmail.com>
2018-06-19 10:49:10 +0800

    refactoring gdpr step definitions

--- src/test/java/steps/GDPRSteps.java
+++ src/test/java/steps/GDPRSteps.java
@@ -9,13 +9,12 @@ public class GDPRSteps {
     private MassiveMailerSite site = new MassiveMailerSite();
     private WebDriverWrapper driver = site.getDriver();
     @When("^I open the main page$")
-    public void iOpenTheMainPage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    public void iOpenTheMainPage() {
        driver.visit(site.baseUrl());
     }
 
     @Then("^System shows GDPR menu on left panel$")
-    public void systemShowsGDPRMenuOnLeftPanel() throws Throwable {
+    public void systemShowsGDPRMenuOnLeftPanel() {
         driver.pageShouldContain("GDPR");
     }
 }
