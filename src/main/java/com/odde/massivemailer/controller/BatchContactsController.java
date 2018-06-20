09ef4b14ce12f2aa977e8c996f29499eece1178a
csd <csd@SevenOfNine.local>
2018-06-20 09:49:05 +0800

    init batch contacts controller

---
+++ src/main/java/com/odde/massivemailer/controller/BatchContactsController.java
@@ -0,0 +1,24 @@
+package com.odde.massivemailer.controller;
+
+import com.odde.massivemailer.model.ContactPerson;
+import com.odde.massivemailer.serialiser.AppGson;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/batchContacts")
+public class BatchContactsController extends AppController {
+    private static final long serialVersionUID = 1L;
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String resultMsg = "";
+        if (req.getContentLength()>0) {
+            resultMsg = "batchContacts.jsp?status=success&msg=Batch add contacts successfully";
+        }
+        resp.sendRedirect(resultMsg);
+    }
+}
---
+++ src/test/java/com/odde/massivemailer/controller/BatchContactsControllerTest.java
@@ -0,0 +1,36 @@
+package com.odde.massivemailer.controller;
+
+import com.odde.TestWithDB;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(TestWithDB.class)
+public class BatchContactsControllerTest {
+    private BatchContactsController controller;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @Before
+    public void setUpMockService() {
+        controller = new BatchContactsController();
+
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+    }
+
+    @Test
+    public void returnListOfValidContacts() throws Exception {
+        String contactPayload = "firstName,lastName;testFN,testLN";
+        request.setContent(contactPayload.getBytes());
+        controller.doPost(request, response);
+
+        assertThat(response.getRedirectedUrl(), is("batchContacts.jsp?status=success&msg=Batch add contacts successfully"));
+    }
+
+}
