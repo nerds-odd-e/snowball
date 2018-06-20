8c93053d385e32128cd175896425212d24b96d58
yhsampei <hsampei@yahoo-corp.jp>
2018-06-20 10:35:27 +0800

    [testcode] add failure testcase for GDPR template

--- src/test/java/com/odde/massivemailer/service/TemplateServiceTest.java
+++ src/test/java/com/odde/massivemailer/service/TemplateServiceTest.java
@@ -3,6 +3,9 @@ package com.odde.massivemailer.service;
 import com.odde.massivemailer.template.TemplateProvider;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Map;
+
 import static org.junit.Assert.assertTrue;
 
 public class TemplateServiceTest {
@@ -12,5 +15,16 @@ public class TemplateServiceTest {
         TemplateService templateService = new TemplateService();
         String resolvedTemplate  = templateService.applyTemplate(TemplateProvider.GDPR_TEMPLATE,null);
         assertTrue("Template should not be empty" , resolvedTemplate.length() > 1);
+    }
+
+    @Test()
+    public void applyNameIntoTemplate() throws Exception{
+        TemplateService templateService = new TemplateService();
+        String name = "abc";
+        Map<String, Object> parameter = Collections.singletonMap("firstName", name);
+
+        String resolvedTemplate  = templateService.applyTemplate(TemplateProvider.GDPR_TEMPLATE, parameter);
+
+        assertTrue("GDPR_TEMPLATE should contain the first name of receipt.", resolvedTemplate.contains(name));
     }
 }
