ecefa259e32c2ec94f793bd97dc303f5bdd48540
yhsampei <hsampei@yahoo-corp.jp>
2018-06-20 10:25:06 +0800

    using GdprService for post action

--- src/main/java/com/odde/massivemailer/controller/GDPRController.java
+++ src/main/java/com/odde/massivemailer/controller/GDPRController.java
@@ -4,6 +4,9 @@ import com.odde.massivemailer.exception.EmailException;
 import com.odde.massivemailer.model.ContactPerson;
 import com.odde.massivemailer.model.Mail;
 import com.odde.massivemailer.serialiser.AppGson;
+import com.odde.massivemailer.service.GDPRService;
+import com.odde.massivemailer.service.TemplateService;
+import com.odde.massivemailer.template.TemplateProvider;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -16,14 +19,11 @@ import java.io.IOException;
 public class GDPRController extends AppController {
     private static final long serialVersionUID = 1L;
 
+    private TemplateService templateService = new TemplateService();
+    private GDPRService gdprService = new GDPRService(mailService, templateService);
+
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ContactPerson.getContactsWithoutConsentRequest().forEach((item) -> {
-            try {
-                mailService.send(null);
-            } catch (EmailException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        gdprService.sendConsentRequestEmail();
         showSuccess(resp);
     }
 
