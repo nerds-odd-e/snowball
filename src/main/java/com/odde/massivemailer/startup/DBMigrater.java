c1aded39be1c97343734de2ac52f2f377673905b
Terry Yin <terry.yinzhe@gmail.com>
2018-08-11 16:05:52 +0800

    simple migration

--- src/main/java/com/odde/massivemailer/startup/DBMigrater.java
+++ src/main/java/com/odde/massivemailer/startup/DBMigrater.java
@@ -1,6 +1,7 @@
 package com.odde.massivemailer.startup;
 
 import org.javalite.activejdbc.Base;
+import org.javalite.activejdbc.DBException;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -11,9 +12,28 @@ import java.util.function.Function;
 
 public class DBMigrater {
     public void migrate() {
+        String last_migration_name = "";
+        String new_last_migration_name = null;
+        try {
+            last_migration_name = (String) Base.firstCell("select max(last_migration_name) from migration_info;");
+            if (last_migration_name == null) {
+                last_migration_name = "";
+            }
+        } catch (DBException e) {
+            System.out.printf("Migration for the first time...\n");
+        }
+
         for (String migration : migrationFiles()) {
-            Arrays.stream(loadMigration(migration).split(";")).filter(s->!s.trim().isEmpty()).forEach(
-                    sql-> Base.exec(sql + ";"));
+            if (migration.compareTo(last_migration_name)> 0) {
+                new_last_migration_name = migration;
+                Arrays.stream(loadMigration(migration).split(";")).filter(s -> !s.trim().isEmpty()).forEach(
+                        sql -> Base.exec(sql + ";"));
+            }
+        }
+
+        if (new_last_migration_name != null && !new_last_migration_name.equals(last_migration_name)) {
+            Base.exec("insert into migration_info (last_migration_name) values (?);", new_last_migration_name);
+
         }
     }
 
---
+++ src/main/resources/db/migration/0000_create_migration_info_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS migration_info (
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    last_migration_name        VARCHAR(255) NOT NULL
+);
