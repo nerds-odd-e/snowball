e717b90ae20fb2bd455485b1a5988c38e27b98d1
Naoto Yoshioka / Taiji Fukaya <yos@pb3.so-net.ne.jp>
2018-08-17 14:07:31 +0900

    refactor error message

--- src/main/java/com/odde/massivemailer/controller/ContactsController.java
+++ src/main/java/com/odde/massivemailer/controller/ContactsController.java
@@ -29,18 +29,15 @@ public class ContactsController extends AppController {
     }
 
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String resultMsg;
 
 		String emailAddress = req.getParameter("email");
         if(!ContactPerson.isValidEmail(emailAddress)) {
-        	resultMsg = "status=fail&msg=error message";
-			resp.sendRedirect("add_contact.jsp?" + resultMsg);
+			resp.sendRedirect("add_contact.jsp?" + "status=fail&msg=error message");
 			return;
         }
 		try {
 			MailService mailService = getMailService();
 			ContactPerson.createContact(req.getParameter("city"), req.getParameter("country"), emailAddress, req.getParameter("name"), req.getParameter("lastname"), req.getParameter("company"));
-			resultMsg = "status=success&msg=Add contact successfully";
 			User user = new User(emailAddress);
 			if (user.saveIt()) {
 				Mail email = new Mail();
@@ -48,10 +45,10 @@ public class ContactsController extends AppController {
 				email.setContent("http://localhost:8060/massive_mailer/initialPassword?token=" + user.getToken());
 				email.sendMailToRecipient(emailAddress, mailService);
 			}
+			resp.sendRedirect("contactlist.jsp?" + "status=success&msg=Add contact successfully");
 		} catch (Exception e) {
-            resultMsg = "status=failed&msg=" + e.getMessage();
+			resp.sendRedirect("contactlist.jsp?" + "status=failed&msg=" + e.getMessage());
 		}
-		resp.sendRedirect("contactlist.jsp?" + resultMsg);
     }
 
 }
