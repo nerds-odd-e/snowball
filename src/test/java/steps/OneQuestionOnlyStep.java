d65590f02bf7f580db4d890c2eb1476f5110c8d6
Shundo Kishi <syundo0730@gmail.com>
2018-08-28 17:59:01 +0900

    add Question Step Test

---
+++ src/test/java/steps/OneQuestionOnlyStep.java
@@ -0,0 +1,116 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class OneQuestionOnlyStep {
+    @Given("^User is student$")
+    public void user_is_student() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User clicks \"([^\"]*)\" button$")
+    public void user_clicks_button(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User go to the test page$")
+    public void user_go_to_the_test_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User should see a question and options$")
+    public void user_should_see_a_question_and_options() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User is in the test page$")
+    public void user_is_in_the_test_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User chooses the correct option$")
+    public void user_chooses_the_correct_option() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User clicks the answer button$")
+    public void user_clicks_the_answer_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User should see the \"([^\"]*)\" page$")
+    public void user_should_see_the_page(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User chooses incorrect option$")
+    public void user_chooses_incorrect_option() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User should see the selected option highlighted in red$")
+    public void user_should_see_the_selected_option_highlighted_in_red() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^The correct option should be highlighted in green$")
+    public void the_correct_option_should_be_highlighted_in_green() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^User should see the advice$")
+    public void user_should_see_the_advice() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User is in advice page$")
+    public void user_is_in_advice_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User clicks next button$")
+    public void user_clicks_next_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^Student as user$")
+    public void student_as_user() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^Student want to take random test$")
+    public void student_want_to_take_random_test() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^System displayed random test question page\\.$")
+    public void system_displayed_random_test_question_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Student pressed \"([^\"]*)\" button\\.$")
+    public void student_pressed_button(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
--- src/test/java/steps/RandomQuestion.java
+++ src/test/java/steps/RandomQuestion.java
@@ -5,40 +5,52 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class RandomQuestion {
-    @Given("^Student as user$")
-    public void student_as_user() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+    @When("^Student choose a correct answer for (\\d+) times \"([^\"]*)\" \"([^\"]*)\"$")
+    public void student_choose_a_correct_answer_for_times(int arg1, String arg2, String arg3) throws Throwable {
     }
 
-    @Given("^Student want to take random test$")
-    public void student_want_to_take_random_test() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^System displayed EndOfTest page$")
+    public void system_displayed_EndOfTest_page() throws Throwable {
     }
 
-    @Given("^System displayed random test question page\\.$")
-    public void system_displayed_random_test_question_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @When("^Student choose a correct answer\\.$")
-    public void student_choose_a_correct_answer() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @When("^Student pressed \"([^\"]*)\" button\\.$")
-    public void student_pressed_button(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @Then("^System displayed next question which choose Randomly$")
-    public void system_displayed_next_question_which_choose_Randomly() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+//    @Given("^Student as user$")
+//    public void student_as_user() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+//    }
+//
+//    @Given("^Student want to take random test$")
+//    public void student_want_to_take_random_test() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+//    }
+//
+//    @Given("^System displayed random test question page\\.$")
+//    public void system_displayed_random_test_question_page() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+//    }
+//
+//    @When("^Student choose a correct answer\\.$")
+//    public void student_choose_a_correct_answer() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+//    }
+//
+//    @When("^Student pressed \"([^\"]*)\" button\\.$")
+//    public void student_pressed_button(String arg1) throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+//    }
+//
+//    @Then("^System displayed next question which choose Randomly$")
+//    public void system_displayed_next_question_which_choose_Randomly() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+//    }
 }
