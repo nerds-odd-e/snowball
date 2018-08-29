375dbf42b4b7f230de95e31d26cca60d5cf4290f
TeamVictory <like.wdsl@gmail.com>
2018-08-29 17:01:25 +0900

    Abstract getting demmy string with description length

--- src/cucumber/resources/features/question_management.feature
+++ src/cucumber/resources/features/question_management.feature
@@ -66,7 +66,7 @@ Feature: Question management
       |5,0,0,0,0     |You need to input at least 2 options|
       |101,10,0,0,0  |Option is over length               |
 
-  @developing
+  @now
   Scenario Outline: Add a new question with invalid advice
     When trainer add a new question with advice that have "<advice length>"
     And option2 is selected as correct answer
--- src/test/java/steps/QuestionStep.java
+++ src/test/java/steps/QuestionStep.java
@@ -2,7 +2,6 @@ package steps;
 
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -16,17 +15,17 @@ public class QuestionStep {
     private WebDriverWrapper driver = site.getDriver();
 
     @When("^User clicks \"([^\"]*)\" button$")
-    public void user_clicks_button(String arg1) throws Throwable {
+    public void user_clicks_button(String arg1) {
         site.visit("question");
     }
 
     @Then("^User go to the test page$")
-    public void user_go_to_the_test_page() throws Throwable {
+    public void user_go_to_the_test_page() {
         driver.expectElementWithIdToContainText("description", "What is scrum?");
     }
 
     @Then("^User should see a question and options$")
-    public void user_should_see_a_question_and_options(DataTable question) throws Throwable {
+    public void user_should_see_a_question_and_options(DataTable question) {
         Map<String, String> questionMap = question.asMap(String.class, String.class);
         driver.expectElementWithIdToContainText("description", questionMap.get("description"));
         driver.expectElementWithIdToContainText("option1", questionMap.get("option1"));
@@ -37,66 +36,57 @@ public class QuestionStep {
     }
 
     @Given("^User is in the test page$")
-    public void user_is_in_the_test_page() throws Throwable {
+    public void user_is_in_the_test_page() {
         site.visit("question");
     }
 
     @When("^User chooses the correct option$")
-    public void user_chooses_the_correct_option() throws Throwable {
+    public void user_chooses_the_correct_option() {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @When("^User clicks the answer button$")
-    public void user_clicks_the_answer_button() throws Throwable {
+    public void user_clicks_the_answer_button() {
         driver.clickButton("answer");
     }
 
     @Then("^User should see the \"([^\"]*)\" page$")
-    public void user_should_see_the_page(String pageName) throws Throwable {
+    public void user_should_see_the_page(String pageName) {
         driver.expectElementWithIdToContainText("title", "end of test");
     }
 
     @When("^User chooses the incorrect option$")
-    public void user_chooses_the_incorrect_option() throws Throwable {
+    public void user_chooses_the_incorrect_option() {
         driver.clickById("option2");
     }
 
     @Then("^User go to the \"([^\"]*)\" page$")
-    public void user_go_to_the_page(String pageName) throws Throwable {
+    public void user_go_to_the_page(String pageName) {
         driver.pageShouldContain(pageName);
     }
 
     @Then("^User should see correct option highlighted in \"([^\"]*)\"$")
-    public void user_should_see_correct_option_highlighted_in(String arg1) throws Throwable {
+    public void user_should_see_correct_option_highlighted_in(String arg1) {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @Then("^User should see selected incorrect option highlighted in \"([^\"]*)\"$")
-    public void user_should_see_selected_incorrect_option_highlighted_in(String arg1) throws Throwable {
+    public void user_should_see_selected_incorrect_option_highlighted_in(String arg1) {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @Then("^User should see \"([^\"]*)\"$")
-    public void user_should_see(String arg1) throws Throwable {
+    public void user_should_see(String arg1) {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @Given("^a trainer enters the question edit page$")
-    public void aTrainerEntersTheQuestionEditPage() throws Throwable {
+    public void aTrainerEntersTheQuestionEditPage() {
         driver.visit(site.baseUrl() + "add_question.jsp");
-    }
-
-    @When("^trainer add a new question with description that have \"([^\"]*)\"$")
-    public void trainerAddANewQuestionWithDescriptionThatHave(int description_length) throws Throwable {
-        String description = "";
-        for (int i = 0; i < description_length; i++) {
-            description += "a";
-        }
-        driver.setTextField("description", description);
     }
 
     private void addQuestionWithDumyData() {
@@ -104,5 +94,19 @@ public class QuestionStep {
         driver.setTextField("option1", "dumy option1");
         driver.setTextField("option2", "dumy option2");
         driver.setTextField("advice", "dumy advice");
+    }
+
+    @When("^trainer add a new question with advice that have \"([^\"]*)\"$")
+    public void trainerAddANewQuestionWithAdviceThatHave(int description_length) {
+        this.addQuestionWithDumyData();
+        driver.setTextField("description", getStringOfLength(description_length));
+    }
+
+    private String getStringOfLength(int description_length) {
+        String description = "";
+        for (int i = 0; i < description_length; i++) {
+            description += "a";
+        }
+        return description;
     }
 }
