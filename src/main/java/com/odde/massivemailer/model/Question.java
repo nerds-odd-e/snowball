89d87d39f97bbdaedfe07bcd6da0c7f5dae0ac0e
CSD User <csduser@Anakins-Mac-mini.local>
2018-09-25 13:52:29 +0900

    add Test modules.

---
+++ src/main/java/com/odde/massivemailer/model/Question.java
@@ -0,0 +1,29 @@
+package com.odde.massivemailer.model;
+
+import java.util.List;
+
+public class Question {
+
+    private String description;
+    private List<Option> options;
+    private String advice;
+
+    public Question(String description, List<Option> options, String advice) {
+        this.description = description;
+        this.options = options;
+        this.advice = advice;
+    }
+
+    public List<Option> getOptions() {
+        return options;
+    }
+
+    public String getAdvice() {
+        return advice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
