0014aceed38650642e869a8e431a8543978bf1de
hirogoe <xdokkoix@gmail.com>
2018-09-25 11:01:37 +0900

    Revert "add test models"
    This reverts commit f3ba5c1fedac280ac78c578cd9f35b93c4299652.

--- src/test/java/com/odde/massivemailer/model/Question.java
+++
@@ -1,8 +0,0 @@
-package com.odde.massivemailer.model;
-
-public class Question {
-    public String description;
-    public List<Choice> Choices;
-    public String advice;
-
-}
--- src/test/java/com/odde/massivemailer/model/Test.java
+++
@@ -1,8 +0,0 @@
-package com.odde.massivemailer.model;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Test {
-    List<Question> questions = new ArrayList<>();
-}
