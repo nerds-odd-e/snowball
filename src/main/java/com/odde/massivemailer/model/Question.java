0eeb92632acc111a814cb757aa03c65bc17b1276
Tomohir0Sano <tomohiro.sano@bizreach.co.jp>
2018-09-26 10:21:42 +0900

    implementation of "judge whether question is answered"

--- src/main/java/com/odde/massivemailer/model/Question.java
+++ src/main/java/com/odde/massivemailer/model/Question.java
@@ -1,17 +1,24 @@
 package com.odde.massivemailer.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Question {
 
     private String description;
     private List<Option> options;
     private String advice;
+    private Long answeredOptionId;
 
     public Question(String description, List<Option> options, String advice) {
+        this(description, options, advice, null);
+    }
+
+    public Question(String description, List<Option> options, String advice, Long answeredOptionId) {
         this.description = description;
         this.options = options;
         this.advice = advice;
+        this.answeredOptionId = answeredOptionId;
     }
 
     public List<Option> getOptions() {
@@ -27,6 +34,6 @@ public class Question {
     }
 
     public boolean isAnswered() {
-        return false;
+        return Objects.nonNull(answeredOptionId);
     }
 }
--- src/test/java/com/odde/massivemailer/model/QuestionTest.java
+++ src/test/java/com/odde/massivemailer/model/QuestionTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QuestionTest {
     @Test
@@ -22,9 +23,19 @@ public class QuestionTest {
     }
 
     @Test
-    public void testAnswered() {
+    public void questionIsAnswered() {
+        Long OptionId = 1L;
+        Option[] options = {new Option(OptionId, "Rugby", false)};
+        Question question = new Question("What is Scrum?", Arrays.asList(options), null, OptionId);
+
+        boolean isAnswered = question.isAnswered();
+        assertTrue(isAnswered);
+    }
+
+    @Test
+    public void questionIsNotAnswered() {
         Option[] options = {new Option(1L, "Rugby", false)};
-        Question question = new Question("What is Scrum?", Arrays.asList(options), null);
+        Question question = new Question("What is Scrum?", Arrays.asList(options), null, null);
 
         boolean isAnswered = question.isAnswered();
         assertFalse(isAnswered);
