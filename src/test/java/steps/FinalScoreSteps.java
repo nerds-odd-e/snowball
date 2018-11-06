a3a68777529d37d3d3de73cc4b88018b3231ef13
CSD User <csduser@Anakins-Mac-mini.local>
2018-11-06 14:04:48 +0900

    add finalScore feature test

---
+++ src/test/java/steps/FinalScoreSteps.java
@@ -0,0 +1,26 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class FinalScoreSteps {
+    @Given("^アドミンがスクラムのカテゴリの問題が(\\d+)問つくられている$")
+    public void アドミンがスクラムのカテゴリの問題が問つくられている(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^ユーザが(\\d+)問正答する$")
+    public void ユーザが問正答する(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^\"([^\"]*)\"が表示されてる事$")
+    public void が表示されてる事(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
