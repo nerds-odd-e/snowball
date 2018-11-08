1f9c1e81f470be85fe89317a3eca52247c8436ea
Goooo <goooo@example.com>
2018-11-08 11:15:42 +0900

    final sore test refactoring

--- src/cucumber/resources/features/online_test/finalscore.feature
+++ src/cucumber/resources/features/online_test/finalscore.feature
@@ -7,17 +7,6 @@ Feature:
     When ユーザが7問正答する
     Then "スクラムをもっと勉強してください"が表示されてる事
 
-  Scenario Outline: 最後の問題が終わったらFinal Scoreを表示する
-    Given 問題が<number_of_questions>問ある時
-    And ユーザーが<number_of_correct>問正解したら
-    Then "<message>" が表示される
-
-    Examples:
-      | number_of_questions | number_of_correct | message                             |
-      | 20                  | 20                | 20/20問 あなたの正解率は100%です。あなたはスクラムマスター！ |
-      | 20                  | 17                | 17/20問 あなたの正解率は85%です。あともう少し！        |
-      | 20                  | 16                | 16/20問 あなたの正解率は80%です。基本を学び直しましょう    |
-
   Scenario Outline: テストが全n問のとき最終ページの分母にnが表示される
     Given User is taking a onlineTest with <number_of_questions> questions
     And  User answered correctly the <number_of_questions> th question page
--- src/test/java/steps/FinalScoreSteps.java
+++ src/test/java/steps/FinalScoreSteps.java
@@ -49,4 +49,22 @@ public class FinalScoreSteps {
         UiElement element = driver.findElementById("message");
         assertEquals(message, element.getText());
     }
+
+    @Then("^分母に(\\d+)が表示される$")
+    public void 分母にが表示される(int totalCount) throws Throwable {
+        UiElement element = driver.findElementById("total-count");
+        assertEquals(String.valueOf(totalCount), element.getText());
+    }
+
+    @Then("^分子に(\\d+)が表示される$")
+    public void 分子にが表示される(int correctCount) throws Throwable {
+        UiElement element = driver.findElementById("correct-count");
+        assertEquals(String.valueOf(correctCount), element.getText());
+    }
+
+    @Then("^正答率に(\\d+)が表示される$")
+    public void 正答率にが表示される(int correctPercentage) throws Throwable {
+        UiElement element = driver.findElementById("correct-percentage");
+        assertEquals(String.valueOf(correctPercentage), element.getText());
+    }
 }
