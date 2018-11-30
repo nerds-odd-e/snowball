1074e6fa01a02b89004dd8776011d55b1c592e92
Tomohir0Sano <tomohiro.sano@bizreach.co.jp>
2018-11-30 11:45:15 +0900

    fix migration trouble for unittest

--- src/main/java/com/odde/massivemailer/startup/DBMigrater.java
+++ src/main/java/com/odde/massivemailer/startup/DBMigrater.java
@@ -4,9 +4,11 @@ import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.DBException;
 
 import java.io.*;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class DBMigrater {
     public void migrate() {
@@ -21,18 +23,26 @@ public class DBMigrater {
             System.out.print("Migration for the first time...\n");
         }
 
-        for (String migration : migrationFiles()) {
-            if (migration.compareTo(last_migration_name)> 0) {
-                new_last_migration_name = migration;
-                Arrays.stream(loadMigration(migration).split(";")).filter(s -> !s.trim().isEmpty()).forEach(
-                        sql -> Base.exec(sql + ";"));
-            }
-        }
+        new_last_migration_name = resetTable(new_last_migration_name);
 
         if (new_last_migration_name != null && !new_last_migration_name.equals(last_migration_name)) {
             Base.exec("insert into migration_info (last_migration_name) values (?);", new_last_migration_name);
 
         }
+    }
+
+    private String resetTable(String new_last_migration_name) {
+        List<Map> tables = Base.findAll("show tables");
+        for (Map table : tables) {
+            Object tableName = table.get("Tables_in_massive_mailer_unittest");
+            Base.exec("drop table " + tableName);
+        }
+        for (String migration : migrationFiles()) {
+                new_last_migration_name = migration;
+                Arrays.stream(loadMigration(migration).split(";")).filter(s -> !s.trim().isEmpty()).forEach(
+                        sql -> Base.exec(sql + ";"));
+        }
+        return new_last_migration_name;
     }
 
     private List<String> migrationFiles() {
