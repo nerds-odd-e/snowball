7ad9d627e412f0365d5ad743725f45333e408c6b
d.kanai <d.kanai@pastoraldog.com>
2018-12-03 18:30:51 +0900

    revert migrate problem

--- src/test/java/steps/driver/DBConnectionFactory.java
+++ src/test/java/steps/driver/DBConnectionFactory.java
@@ -1,12 +1,6 @@
 package steps.driver;
 
-import com.odde.TestWithDB;
-import com.odde.massivemailer.startup.DBMigrater;
 import org.javalite.activejdbc.Base;
-import org.junit.runners.model.InitializationError;
-
-import java.util.List;
-import java.util.Map;
 
 public class DBConnectionFactory {
     public static void prepare() {
@@ -15,8 +9,15 @@ public class DBConnectionFactory {
     }
 
     private static void cleanDB() {
-        List<Map> tables = Base.findAll("show tables");
-        tables.forEach(table->{table.values().stream().filter(name-> name != "migration_info").forEach(name ->{ Base.exec("delete from " + name); });});
+        Base.exec("DELETE FROM template");
+        Base.exec("DELETE FROM sent_mail_visits");
+        Base.exec("DELETE FROM sent_mails");
+        Base.exec("DELETE FROM COURSE_CONTACT_PERSON");
+        Base.exec("DELETE FROM contact_people");
+        Base.exec("DELETE FROM courses");
+        Base.exec("DELETE FROM questions");
+        Base.exec("DELETE FROM question_responses");
+        Base.exec("DELETE FROM options");
         Base.exec("INSERT INTO template (TemplateName,Subject,Content) VALUES ('Default Template 1', 'Greeting {FirstName}', 'Hi, {FirstName} {LastName} from {Company}')");
         Base.exec("INSERT INTO template (TemplateName,Subject,Content) VALUES ('RTA Default Template', 'Greeting {FirstName}', 'Hi, {FirstName} {LastName} from {Company}');");
         Base.exec("INSERT INTO template (TemplateName,Subject,Content) VALUES ('Pre-course Template', 'Greeting {FirstName}', 'Hi, {FirstName} {LastName} from {Company}');");
