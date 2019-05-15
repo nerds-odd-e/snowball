100acba1907ca9eb45d29752fa4c9fec97ade859
peace <peace@csd.fun>
2019-05-15 14:23:39 +0900

    add feature

--- src/cucumber/resources/features/tokkun/display_wrong_tokkun_question.feature
+++ src/cucumber/resources/features/tokkun/display_wrong_tokkun_question.feature
@@ -1,8 +1,9 @@
 Feature:
   display wrong tokkun question one day later
 
-  Scenario Outline:
-    Given 問題が登録されている
+  @now
+  Scenario Outline: 間違った問題は22時間後に表示される
+    Given Given Add a question "スクラムとは何ですか？" with dummy options and advice "Read the Scrum Guide again, please"
     When 問題に<is_answer_correct>する
     And <elapsed_hour>時間が経過する
     Then 問題が<is_display>される
---
+++ src/test/java/steps/WrongQuestionSteps.java
@@ -0,0 +1,36 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import steps.driver.WebDriverWrapper;
+import steps.site.MassiveMailerSite;
+
+public class WrongQuestionSteps {
+    private final MassiveMailerSite site = new MassiveMailerSite();
+    private final WebDriverWrapper driver = site.getDriver();
+
+    @Given("^Given Add a question \"([^\"]*)\" with dummy options and advice \"([^\"]*)\"$")
+    public void given_Add_a_question_with_dummy_options_and_advice(String arg1, String arg2) {
+    }
+
+    @When("^問題に正解する$")
+    public void 問題に正解する() {
+    }
+
+    @When("^問題に不正解する$")
+    public void 問題に不正解する() {
+    }
+
+    @Then("^問題が非表示される$")
+    public void 問題が非表示される() {
+    }
+
+    @Then("^問題が表示される$")
+    public void 問題が表示される() {
+    }
+
+    @When("^(\\d+)時間が経過する$")
+    public void 時間が経過する(int arg1) {
+    }
+}
