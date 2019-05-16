8379a9bed177b323448d1190bd54564714b68c58
mine0321 <yoshifumi.hayashi@groove-x.com>
2019-05-16 10:12:36 +0900

    add steps of admin approves public question

---
+++ src/test/java/steps/AdminApprovePublicQuestionSteps.java
@@ -0,0 +1,26 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class AdminApprovePublicQuestionSteps {
+
+    @Given("^未承認の問題がある$")
+    public void 未承認の問題がある() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+    }
+
+    @When("^Adminが承認する$")
+    public void adminが承認する() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+    }
+
+    @Then("^問題が承認済みになる$")
+    public void 問題が承認済みになる() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+    }
+}
