e31fea47eb7dec8efc516ca74ed29439c1389bcf
d.kanai <d.kanai@pastoraldog.com>
2019-06-11 17:14:28 +0900

    refactoring

--- src/main/java/com/odde/massivemailer/controller/AppController.java
+++ src/main/java/com/odde/massivemailer/controller/AppController.java
@@ -1,8 +1,32 @@
 package com.odde.massivemailer.controller;
 
+import com.google.gson.Gson;
+import org.javalite.activejdbc.LazyList;
+import org.javalite.activejdbc.Model;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AppController extends HttpServlet {
+
+    public Map getRequestBody(HttpServletRequest req) throws IOException {
+        String result = req.getReader().lines().collect(Collectors.joining("\r\n"));
+        return new Gson().fromJson(result, Map.class);
+    }
+
+    public void jsonResponse(HttpServletResponse resp, String s) throws IOException {
+        resp.setHeader("Content-Type", "application/json; charset=UTF-8");
+        resp.getOutputStream().print(s);
+    }
+
+    public String toJson(LazyList<Model> objects) {
+        String json = String.join(",", objects.stream().map(obj -> obj.toJson(true)).collect(Collectors.toList()));
+        return "[" + json + "]";
+    }
 
 
 }
--- src/main/java/com/odde/massivemailer/controller/TodosController.java
+++ src/main/java/com/odde/massivemailer/controller/TodosController.java
@@ -1,27 +1,17 @@
 package com.odde.massivemailer.controller;
 
 import com.odde.massivemailer.model.Todo;
-import org.javalite.activejdbc.LazyList;
-import org.javalite.activejdbc.Model;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 @WebServlet("/todos")
 public class TodosController extends AppController {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setHeader("Content-Type", "application/json; charset=UTF-8");
-        resp.getOutputStream().print(toJson(Todo.findAll()));
+        jsonResponse(resp, toJson(Todo.findAll()));
     }
 
-    private String toJson(LazyList<Model> objects) {
-        String json = String.join(",", objects.stream().map(obj -> obj.toJson(true)).collect(Collectors.toList()));
-        return "[" + json + "]";
-    }
 }
