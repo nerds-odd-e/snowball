5fb27652eaba307b7351e88129abe45dadf72d5e
d.kanai <d.kanai@pastoraldog.com>
2019-06-12 14:09:37 +0900

    new test

--- frontend/test/unit/specs/Todos.spec.js
+++ frontend/test/unit/specs/Todos.spec.js
@@ -3,9 +3,11 @@ import Todos from '@/components/Todos'
 
 
 describe('todos', () => {
-  it('should render correct contents', () => {
+  it('render h1', () => {
     const Constructor = Vue.extend(Todos)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('#todos h1').textContent).toEqual('Todos List')
+  })
+  it('render todo', () => {
   })
 })
