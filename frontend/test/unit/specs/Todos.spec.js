2dd7bc34336396b7f8cd33851b1c2be6c430ff18
d.kanai <d.kanai@pastoraldog.com>
2019-06-12 15:16:15 +0900

    refactor

--- frontend/test/unit/specs/Todos.spec.js
+++ frontend/test/unit/specs/Todos.spec.js
@@ -4,19 +4,19 @@ import * as flushPromises from "flush-promises";
 
 
 describe('todos', () => {
-//  beforeEach(() => {
-//    fetch.resetMocks()
-//  })
-//  it('render h1', () => {
-//    const Constructor = Vue.extend(Todos)
-//    const vm = new Constructor().$mount()
-//    expect(vm.$el.querySelector('#todos h1').textContent).toEqual('Todos List')
-//  })
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+  it('render h1', () => {
+    const Constructor = Vue.extend(Todos)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('#todos h1').textContent).toEqual('Todos List')
+  })
   it('render todo', (done) => {
     expect(Todos.data()['todos']).toEqual([])
 
     let json = [{
-      "id":1,
+      "id": 1,
       "status":"todo",
       "title":"first todo"
     }]
@@ -25,18 +25,17 @@ describe('todos', () => {
     const Constructor = Vue.extend(Todos)
     const vm = new Constructor().$mount()
 
-    vm.$nextTick()
-    .then(() => {
-        vm.$nextTick().then(() => {
-          expect(fetch.mock.calls.length).toEqual(1)
-          expect(fetch.mock.calls[0][0]).toEqual('http://localhost:8070/todos')
-          expect(vm.todos).toEqual(json)
+    vm.$nextTick().then(() => {
+      vm.$nextTick().then(() => {
+        expect(fetch.mock.calls.length).toEqual(1)
+        expect(fetch.mock.calls[0][0]).toEqual('http://localhost:8070/todos')
+        expect(vm.todos).toEqual(json)
 
-          vm.$nextTick().then(() => {
-            expect(vm.$el.querySelector('.todo').textContent).toEqual('first todo todo')
-            done();
-          })
-        });
+        vm.$nextTick().then(() => {
+          expect(vm.$el.querySelector('.todo').textContent).toEqual('first todo todo')
+          done();
+        })
+      });
     })
   })
 })
