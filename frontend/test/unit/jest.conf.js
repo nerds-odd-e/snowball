8b80bc7b7a683501dc7f47a737246b06ffeadd38
d.kanai <d.kanai@pastoraldog.com>
2019-06-12 11:08:10 +0900

    fix js test config

--- frontend/test/unit/jest.conf.js
+++ frontend/test/unit/jest.conf.js
@@ -10,13 +10,13 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
+  testURL: "http://localhost:8080",
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
