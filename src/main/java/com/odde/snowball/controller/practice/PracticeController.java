9f78bca169f72f6515b022e9e7bfa467eeb98f5b
coffeebenzene <ericteo_no1@hotmail.com>
2019-07-02 16:58:47 +0800

    Practice ensurePractice

--- src/main/java/com/odde/snowball/controller/practice/PracticeController.java
+++ src/main/java/com/odde/snowball/controller/practice/PracticeController.java
@@ -2,11 +2,18 @@ package com.odde.snowball.controller.practice;
 
 
 import com.odde.snowball.controller.AppController;
+import com.odde.snowball.model.practice.Practice;
+import org.bson.types.ObjectId;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 @WebServlet("/practice/practice")
 public class PracticeController extends AppController {
@@ -15,4 +22,14 @@ public class PracticeController extends AppController {
 
     }
 
+    public Practice ensurePractice(ObjectId userId) {
+        Practice practice = Practice.fetchPracticeByUserId(userId);
+        if (practice == null){
+            Collection<ObjectId> categories = new ArrayList<>();
+            List<Integer> cycle = Arrays.asList(1, 3, 7);
+            practice = new Practice(userId, 1, categories, cycle);
+            practice.save();
+        }
+        return practice;
+    }
 }
--- src/test/java/com/odde/snowball/controller/practice/PracticeControllerTest.java
+++ src/test/java/com/odde/snowball/controller/practice/PracticeControllerTest.java
@@ -1,35 +1,58 @@
 package com.odde.snowball.controller.practice;
 
 import com.odde.TestWithDB;
+import com.odde.snowball.model.practice.Practice;
+import org.bson.types.ObjectId;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import static org.junit.Assert.*;
+
 @RunWith(TestWithDB.class)
 public class PracticeControllerTest {
+    private final ObjectId userId = new ObjectId();
 
     private PracticeController controller;
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
 
+    private void createPractice() {
+        Collection<ObjectId> categories = new ArrayList<>();
+        List<Integer> cycle = Arrays.asList(1, 3, 7);
+
+        Practice practice = new Practice(userId, 1, categories, cycle);
+        practice.save();
+    }
 
     @Before
     public void setUpMockService() {
         controller = new PracticeController();
         request = new MockHttpServletRequest();
         response = new MockHttpServletResponse();
+        request.getSession().setAttribute("userId", userId);
     }
 
     @Test
     public void retrievePracticeIfAlreadyExists() {
-
+        createPractice();
+        Practice practice = controller.ensurePractice(userId);
+        assertNotNull(practice);
+        assertEquals(userId, practice.getUserId());
     }
 
     @Test
     public void createPracticeIfDoesNotExist() {
-
+        Practice practice = controller.ensurePractice(userId);
+        assertNotNull(practice);
     }
+
 
 }
