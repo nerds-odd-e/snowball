5fb6af716205ad68f23e8931bb0e00a1c69e97e4
HORI <csduser@luke.local>
2019-08-28 10:05:41 +0900

    spaceBasedRepetation用問取得メソッド追加

--- src/main/java/com/odde/snowball/model/onlinetest/OnlinePractice.java
+++ src/main/java/com/odde/snowball/model/onlinetest/OnlinePractice.java
@@ -2,6 +2,8 @@ package com.odde.snowball.model.onlinetest;
 
 import com.odde.snowball.model.User;
 
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +19,11 @@ public class OnlinePractice extends OnlineTest {
         List<Question> dueQuestions = allQuestions.stream().filter(q-> q.isDueForUser(user)).collect(Collectors.toList());
         List<Question> questions = new QuestionCollection(dueQuestions).generateQuestionList(repo(Category.class).findAll(), dueQuestions.size());
         return new OnlinePractice(questions);
+    }
+
+    public static List<Question> findSpaceBasedRepetations(int count, User user, Date currentDate) {
+        List<Question> questions = repo(Question.class).findAll();
+        return Collections.emptyList();
     }
 
     public String endPageName() {
