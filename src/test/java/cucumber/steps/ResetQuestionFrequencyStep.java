8fde2fd39deb68bfba282b969179a6d57d3570af
warren, umegaki <a@a.com>
2019-11-13 14:40:48 +0900

    fix senario for reset question

--- src/test/java/cucumber/steps/ResetQuestionFrequencyStep.java
+++ src/test/java/cucumber/steps/ResetQuestionFrequencyStep.java
@@ -1,11 +1,16 @@
 package cucumber.steps;
 
+import com.odde.snowball.factory.QuestionBuilder;
 import com.odde.snowball.model.User;
+import com.odde.snowball.model.onlinetest.Question;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.steps.driver.WebDriverWrapper;
 import cucumber.steps.site.SnowballSite;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResetQuestionFrequencyStep {
     private final SnowballSite site = new SnowballSite();
@@ -13,6 +18,8 @@ public class ResetQuestionFrequencyStep {
     private final String login_url = site.baseUrl() + "login.jsp";
     private final User user = createUser("john@example.com");
     private final String password = "abcd1002";
+    private int numberOfCorrectAnsweredQuestion;
+    private List<Question> questions = new ArrayList<Question>();
 
     private User createUser(String email) {
         User user = new User("john@example.com");
@@ -31,45 +38,43 @@ public class ResetQuestionFrequencyStep {
         driver.click("#login");
     }
 
-    @Given("There are {int} questions")
-    public void there_are_questions(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
-    }
-
     @Given("The user start the practice")
     public void the_user_start_the_practice() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        driver.click("#start_practice_button");
     }
 
     @Given("The user answer {int} correctly")
-    public void the_user_answer_correctly(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void the_user_answer_correctly(Integer correctCount) {
+        this.numberOfCorrectAnsweredQuestion = correctCount;
+        for (int i = 0; i < correctCount; ++i) {
+            driver.clickRadioButton("correct");
+            driver.click("#answer");
+        }
     }
 
     @Given("The user answer {int} incorrectly")
-    public void the_user_answer_incorrectly(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void the_user_answer_incorrectly(Integer incorrectCount) {
+        for (int i = 0; i < incorrectCount; ++i) {
+            driver.clickRadioButton("Food");
+            driver.click("#answer");
+            driver.click("#next");
+        }
     }
 
     @Given("The user has seen the {string}")
-    public void the_user_has_seen_the(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void the_user_has_seen_the(String text) {
+        driver.expectPageToContainText(text);
     }
 
     @When("User start the practice again")
     public void user_start_the_practice_again() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        driver.click("#home");
+        driver.expectURLToContain("/dashboard");
+        driver.click("#start_practice_button");
     }
 
     @Then("User only sees the same question that was answer incorrectly")
     public void user_only_sees_the_same_question_that_was_answer_incorrectly() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        driver.expectPageToContainText("What is PO?");
     }
 }
--- src/test/resources/features/practice/reset_question_frequency.feature
+++ src/test/resources/features/practice/reset_question_frequency.feature
@@ -3,7 +3,10 @@ Feature: The frequency of a question will be set to the base level if the user a
   @developing
   Scenario: If there are 2 questions and the user answer 1 of it correctly, the user see the incorrect one in next practice
     Given There is a logged in user
-    And There are 2 questions
+    Given There are questions with dummy options:
+      | description | advice                             | category | correctOption |
+      | スクラムとは何ですか？ | Read the Scrum Guide again, please | Scrum    | correct |
+      | What is PO? | PO is Product Owner | Scrum | correct |
     And The user start the practice
     And The user answer 1 correctly
     And The user answer 1 incorrectly
