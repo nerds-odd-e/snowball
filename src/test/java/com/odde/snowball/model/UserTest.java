d923c492e7a98fb691b493cdbcaf636d05024ae2
ue hashimoto <a@a>
2020-10-14 15:20:53 +0900

    汎用的なテストメソッドを追加

--- src/test/java/com/odde/snowball/model/UserTest.java
+++ src/test/java/com/odde/snowball/model/UserTest.java
@@ -1,14 +1,11 @@
 package com.odde.snowball.model;
 
 import com.odde.TestWithDB;
-import com.odde.snowball.model.onlinetest.AnswerInfo;
-import com.odde.snowball.model.onlinetest.Category;
-import com.odde.snowball.model.onlinetest.Question;
+import com.odde.snowball.model.onlinetest.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 import static com.odde.snowball.model.base.Repository.repo;
 import static org.junit.Assert.*;
@@ -21,6 +18,42 @@ public class UserTest {
         user.setupPassword(password);
         user.save();
         return user;
+    }
+
+    private User createUser() {
+        String password = "hogehoge";
+        return createUser("answer_info2@example.com", password);
+    }
+
+    private Category createCategory() {
+        Category category = new Category("xp", "", "");
+        category.save();
+        return category;
+    }
+
+    private Question createQuestion() {
+        Category category = createCategory();
+        Question question = new Question("test", "advice",category.getId(), false, true);
+        QuestionOption correctOption = question.addOption(new QuestionOption("これは正解の選択肢", true));
+        QuestionOption incorrectOption = question.addOption(new QuestionOption("これは不正解の選択肢", false));
+        question.addOption(correctOption);
+        question.addOption(incorrectOption);
+        question.save();
+        return question;
+    }
+
+    private Answer createAnswer() {
+        List<String> selectedOptionIds = new ArrayList<>();
+        selectedOptionIds.add("これが正解の選択肢");
+        Question question = createQuestion();
+        Answer answer = new Answer(question, selectedOptionIds);
+        return answer;
+    }
+
+    private Date createDate(int year, int month, int date) {
+        Calendar calender = Calendar.getInstance();
+        calender.set(year, month, date);
+        return  calender.getTime();
     }
 
     @Test
